4911cb7814eb3af84f91245ad166e9b09a20074d
Tarick <gabre.tarick@gmail.com>
2022-02-02 18:35:36 -0300

    add typeof em variavel inexistente teste

--- 01-fundamentos/bloco-04-introcao-a-javascript-logica-de-programacao/Dia_1-javascript-primeiros-passos/exercicio2.js
+++ 01-fundamentos/bloco-04-introcao-a-javascript-logica-de-programacao/Dia_1-javascript-primeiros-passos/exercicio2.js
@@ -1,4 +1,4 @@
-let patientId = 50;
+let patientId = '50';
 let isEnrolled = true;
 const patientInfo = {
   firstName: 'Ana',
@@ -10,3 +10,4 @@ console.log(typeof patientId);
 console.log(typeof isEnrolled);
 console.log(typeof patientInfo);
 console.log(typeof patientEmail);
+console.log(typeof patientAge);
