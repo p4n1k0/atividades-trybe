d6f957adb06657bc2f0865f4ff07f3ddae7b77eb
Tarick <gabre.tarick@gmail.com>
2022-02-03 00:44:02 -0300

    cria programa p achar maior de 3

---
+++ 01-fundamentos/bloco-04-introcao-a-javascript-logica-de-programacao/Dia_1-javascript-primeiros-passos/maiodetres.js
@@ -0,0 +1,15 @@
+const n1 = 8;
+const n2 = 10;
+const n3 = 7;
+
+if(n1 > n2 && n1 > n3){
+    console.log(n1);
+}
+
+else if(n1 < n2 && n2 > n3){
+    console.log(n2);
+}
+
+else if(n1 < n2 && n2 < n3){
+    console.log(n3)
+}
