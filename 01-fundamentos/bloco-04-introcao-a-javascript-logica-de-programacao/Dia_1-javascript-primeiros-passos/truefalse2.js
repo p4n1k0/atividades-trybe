0755ce30212d8fb13c4c5b17aca390cfe7bd0ae0
Tarick <gabre.tarick@gmail.com>
2022-02-03 01:11:57 -0300

    add programa true or false

---
+++ 01-fundamentos/bloco-04-introcao-a-javascript-logica-de-programacao/Dia_1-javascript-primeiros-passos/truefalse2.js
@@ -0,0 +1,18 @@
+const a = 0;
+const b = 0;
+const c = 0;
+
+if(a + b + c === 180){
+    console.log(true);
+}
+
+else if(a + b + c !== 180){
+    console.log(false);
+}
+
+else {
+    console.log("angulo inválido")
+}
+
+
+
