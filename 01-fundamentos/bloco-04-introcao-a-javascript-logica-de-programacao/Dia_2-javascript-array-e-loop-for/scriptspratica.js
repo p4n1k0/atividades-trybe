13802fd6db836362ecb71d79a9afe35a03cf7988
Tarick <gabre.tarick@gmail.com>
2022-02-04 17:31:47 -0300

    conclui exercicio 5

--- 01-fundamentos/bloco-04-introcao-a-javascript-logica-de-programacao/Dia_2-javascript-array-e-loop-for/scriptspratica.js
+++ 01-fundamentos/bloco-04-introcao-a-javascript-logica-de-programacao/Dia_2-javascript-array-e-loop-for/scriptspratica.js
@@ -1,16 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
 
-for (let index = 0; index < numbers.length; index += 1){
-    soma += numbers[index];
+let maior = 0;
+
+for(let index = 0; index < numbers.length; index += 1){
+    if (numbers[index] > maior){
+        maior = numbers[index]
+    }
+
 }
 
-let media = soma / numbers.length;
-
-if(media > 20){
-    console.log("valor maior que 20"); 
-}else{
-    console.log("valor menor que 20");
-}
-
-console.log(media); 
+console.log(maior);
