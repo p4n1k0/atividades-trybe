fa131a3edf3dab6573c7d69178f5c22d6bf02156
Tarick <gabre.tarick@gmail.com>
2022-02-07 22:27:08 -0300

    faz se tornar funçao

--- 01-fundamentos/bloco-04-introcao-a-javascript-logica-de-programacao/Dia_1-javascript-primeiros-passos/operacao.js
+++ 01-fundamentos/bloco-04-introcao-a-javascript-logica-de-programacao/Dia_1-javascript-primeiros-passos/operacao.js
@@ -1,8 +1,25 @@
-const a = 4;
-const b = 7;
+function soma(a, b){
+    return a + b;
+}
 
-console.log('Soma:' + (a + b));
-console.log('Subtração:' + (a - b));
-console.log('Multiplicação:' + (a * b));
-console.log('Divisão' + (a / b));
-console.log('Módulo:' + (a % b));
+function subtracao(a, b){
+    return a - b;
+}
+
+function multiplicacao(a, b){
+    return a * b;
+}
+
+function divisao(a, b){
+    return a / b;
+}
+
+function modulo(a, b){
+    return a % b;
+}
+
+console.log(soma(2, 5));
+console.log(subtracao(7, 5));
+console.log(multiplicacao(3, 3));
+console.log(divisao(25, 5));
+console.log(modulo(10, 5));
