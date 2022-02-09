f12b5ab7c843e4a5c185dbc8fab877ef8469f48b
Tarick <gabre.tarick@gmail.com>
2022-02-09 19:24:36 -0300

    cria funcoes para manipular html

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_1-javascript-dom-e-seletores/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_1-javascript-dom-e-seletores/script.js
@@ -4,3 +4,25 @@ function mudaCor (novaCor){
 }
 
 mudaCor ("green");
+
+
+function corMuda (corNova){
+    let el = document.getElementsByTagName("section")[0];
+    el.style.background = corNova;
+}
+
+corMuda ("salmon");
+
+function changeColor (newColor){
+    let element = document.getElementsByTagName("section")[1];
+    element.style.background = newColor;
+}
+
+changeColor ("gray");
+
+function colorMuda (colorNew){
+    let elem = document.getElementById("footer-container");
+    elem.style.background = colorNew;
+}
+
+colorMuda ("blue");
