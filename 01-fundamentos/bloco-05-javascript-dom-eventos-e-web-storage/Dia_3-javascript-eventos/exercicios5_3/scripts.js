e67015f84f6f3c1b9d3fd37986a2bfedb90fb7c8
Tarick <gabre.tarick@gmail.com>
2022-02-14 19:41:33 -0300

    conclui exercicio 1

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_3-javascript-eventos/exercicios5_3/scripts.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_3-javascript-eventos/exercicios5_3/scripts.js
@@ -14,3 +14,33 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+  let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  function diasDezembro () {
+      let diasDaLista = document.querySelector('#days');
+
+      for (let i = 0; i < dezDaysList.length; i += 1) {
+          let day = dezDaysList[i];
+          let diasLista = document.createElement('li')
+          
+          if (day === 24 || day === 31) {
+            diasLista.className = 'day holiday';
+            diasLista.innerHTML = day;
+            diasDaLista.appendChild(diasLista);
+          } else if (day === 4 || day === 11 || day === 18) {
+            diasLista.className = 'day friday';
+            diasLista.innerHTML = day;
+            diasDaLista.appendChild(diasLista)
+          } else if (day === 25) {
+            diasLista.className = 'day holiday friday';
+            diasLista.innerHTML = day;
+            diasDaLista.appendChild(diasLista);
+          } else {
+            diasLista.innerHTML = day;
+            diasLista.className = 'day';
+            diasDaLista.appendChild(diasLista);
+          };
+      };
+  }
+  diasDezembro();
