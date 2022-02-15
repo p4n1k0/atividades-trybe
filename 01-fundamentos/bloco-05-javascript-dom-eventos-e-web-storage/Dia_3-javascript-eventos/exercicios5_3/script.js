051279160d4a8aa9faeb53e9bdf98c6b1515773c
Tarick <gabre.tarick@gmail.com>
2022-02-14 21:45:57 -0300

    conclui exercicio 8

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_3-javascript-eventos/exercicios5_3/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_3-javascript-eventos/exercicios5_3/script.js
@@ -20,28 +20,28 @@ function createDaysOfTheWeek() {
   let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
   function diasDezembro () {
-      let diasDaLista = document.querySelector('#days');
+      let pegaDiasLista = document.querySelector('#days');
 
       for (let i = 0; i < dezDaysList.length; i += 1) {
           let day = dezDaysList[i];
-          let diasLista = document.createElement('li')
+          let diaLista = document.createElement('li')
           
           if (day === 24 || day === 31) {
-            diasLista.className = 'day holiday';
-            diasLista.innerHTML = day;
-            diasDaLista.appendChild(diasLista);
+            diaLista.className = 'day holiday';
+            diaLista.innerHTML = day;
+            pegaDiasLista.appendChild(diaLista);
           } else if (day === 4 || day === 11 || day === 18) {
-            diasLista.className = 'day friday';
-            diasLista.innerHTML = day;
-            diasDaLista.appendChild(diasLista)
+            diaLista.className = 'day friday';
+            diaLista.innerHTML = day;
+            pegaDiasLista.appendChild(diaLista)
           } else if (day === 25) {
-            diasLista.className = 'day holiday friday';
-            diasLista.innerHTML = day;
-            diasDaLista.appendChild(diasLista);
+            diaLista.className = 'day holiday friday';
+            diaLista.innerHTML = day;
+            pegaDiasLista.appendChild(diaLista);
           } else {
-            diasLista.innerHTML = day;
-            diasLista.className = 'day';
-            diasDaLista.appendChild(diasLista);
+            diaLista.innerHTML = day;
+            diaLista.className = 'day';
+            pegaDiasLista.appendChild(diaLista);
           };
       };
   }
@@ -56,7 +56,7 @@ function createDaysOfTheWeek() {
     let novoBotaoId = 'btn-holiday';
 
     novoBotao.innerHTML = nomeBotao;
-    novoBotaoId.id = novoBotaoId;
+    novoBotao.id = novoBotaoId;
     containerBotao.appendChild(novoBotao);
   };
 
@@ -70,11 +70,11 @@ function mostraFeriados() {
   let achaNovaCor = 'white';
 
   pegaBotaoFeriado.addEventListener('click', function() {
-    for (let i = 0; i < pegaBotaoFeriado.length; i += 1) {
-      if (pegaBotaoFeriado[i].style.backgroundColor === achaNovaCor) {
-        pegaBotaoFeriado[i].styke.backgroundColor = bgColor;
+    for (let i = 0; i < pegaFeriados.length; i += 1) {
+      if (pegaFeriados[i].style.backgroundColor === achaNovaCor) {
+        pegaFeriados[i].style.backgroundColor = bgColor;
       } else {
-        pegaBotaoFeriado[i].style.backgroundColor = achaNovaCor;
+        pegaFeriados[i].style.backgroundColor = achaNovaCor;
       }
     }
   })
@@ -136,4 +136,15 @@ function diaMouseOut() {
 
 diaMouseOver();
 diaMouseOut();
+
+//7. ref: gabarito
+function newTaskSpan(task) {
+  let tasksContainer = document.querySelector('.my-tasks');
+  let nomeTask = document.createElement('span');
+
+  nomeTask.innerHTML = task;
+  tasksContainer.appendChild(nomeTask);
+};
+
+newTaskSpan('Projeto');
 
