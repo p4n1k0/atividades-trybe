5349601745bb7d1846e9a63f56ff2875dafd64e5
Tarick <gabre.tarick@gmail.com>
2022-02-18 17:53:18 -0300

    corrige exercicio 3

--- 01-fundamentos/bloco-06-html-e-css-forms-flexbox-responsivo/teste/script.js
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-responsivo/teste/script.js
@@ -3,19 +3,19 @@ const INPUT_TEXT = document.querySelector("#input-text");
 const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 const HREF_LINK = document.querySelector("#href");
 
-HREF_LINK.addEventListener('click', (event) =>{
+HREF_LINK.addEventListener('click', (event) => {
     event.preventDefault();
 });
 
 INPUT_CHECKBOX.addEventListener('click', (event) => {
     event.preventDefault();
-})  
+});
 
 INPUT_TEXT.addEventListener('keypress', (event) => {
     event.preventDefault();
-    const letra = event.key;
-    if (letra !== 'a') {
-        event.preventDefault;
+    const caractere = event.key;
+    if (caractere !== 'a') {
+        event.preventDefault();
     }
 })
 
