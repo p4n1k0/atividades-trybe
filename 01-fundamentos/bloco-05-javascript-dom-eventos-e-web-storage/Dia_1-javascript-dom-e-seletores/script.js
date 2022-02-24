058aab83a7883e51ad14ce087ab5af17209f656f
Tarick <gabre.tarick@gmail.com>
2022-02-24 11:37:54 -0300

    busca elementos e coloca bg em ambos

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_1-javascript-dom-e-seletores/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_1-javascript-dom-e-seletores/script.js
@@ -1,6 +1,16 @@
 const headerContainer = document.querySelector('#header-container');
 headerContainer.style.background = 'green';
-const emergencyTasks = document.querySelector ('.emergency-tasks');
+const emergencyTasks = document.querySelector('.emergency-tasks');
 emergencyTasks.style.background = 'salmon';
 const noEmergencyTasks = document.querySelector('.no-emergency-tasks');
 noEmergencyTasks.style.background = '#ffd700';
+
+const emergencyTasksHeaders = document.querySelectorAll('.emergency-tasks h3');
+for (let i = 0; i < emergencyTasksHeaders.length; i += 1) {
+  emergencyTasksHeaders[i].style.background = 'purple';
+}
+
+const noEmergencyTasksHaders = document.querySelectorAll('.no-emergency-tasks h3');
+for (let i = 0; i < noEmergencyTasksHaders.length; i += 1) {
+    noEmergencyTasksHaders[i].style.background = 'black';
+}
