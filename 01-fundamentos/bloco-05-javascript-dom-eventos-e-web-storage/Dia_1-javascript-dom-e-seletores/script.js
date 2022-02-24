f9902b248ca1d4bae9d8b7871e942b1667ac934a
Tarick <gabre.tarick@gmail.com>
2022-02-24 01:02:37 -0300

    coloca bg na section noemergencytasks

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_1-javascript-dom-e-seletores/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_1-javascript-dom-e-seletores/script.js
@@ -2,3 +2,5 @@ const headerContainer = document.querySelector('#header-container');
 headerContainer.style.background = 'green';
 const emergencyTasks = document.querySelector ('.emergency-tasks');
 emergencyTasks.style.background = 'salmon';
+const noEmergencyTasks = document.querySelector('.no-emergency-tasks');
+noEmergencyTasks.style.background = '#ffd700';
