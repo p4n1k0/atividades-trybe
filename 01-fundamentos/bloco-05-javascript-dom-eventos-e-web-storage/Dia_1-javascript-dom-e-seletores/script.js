7d4ef2f2759c103898762b159d8fcffc4aae18af
Tarick <gabre.tarick@gmail.com>
2022-02-24 00:54:55 -0300

    coloca bg no emergency-tasks

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_1-javascript-dom-e-seletores/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_1-javascript-dom-e-seletores/script.js
@@ -1,2 +1,4 @@
 const headerContainer = document.querySelector('#header-container');
 headerContainer.style.background = 'green';
+const emergencyTasks = document.querySelector ('.emergency-tasks');
+emergencyTasks.style.background = 'salmon';
