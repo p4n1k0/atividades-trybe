3c3e0f9094ba7afdc9a345af408cbbfab3d12077
Tarick <gabre.tarick@gmail.com>
2022-03-03 20:20:33 -0300

    usa sort para ordenar array

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/arrowfunction.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/arrowfunction.js
@@ -12,20 +12,17 @@ const testingScope = escopo => {
   testingScope(false);
 
 
-
+  
   const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
   // Seu código aqui.
-  const sortOddAndEvans = () => {
-    oddsAndEvens[0] = 2;
-    oddsAndEvens[1] = 3;
-    oddsAndEvens[2] = 4;
-    oddsAndEvens[3] = 7;
-    oddsAndEvens[4] = 10;
-    oddsAndEvens[5] = 13;
-
-    return oddsAndEvens;
+  const sortArrayBonus = array => {
+    const sortOddsAndEvens = array.sort((a, b) => a - b);
+    return sortOddsAndEvens;
   }
-  
-  const sortedArray = sortOddAndEvans();
-  console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente!`); 
+
+  const sortedArrayBonus = sortArrayBonus(oddsAndEvens);
+  console.log(`Os números ${sortedArrayBonus} se encontram ordenados de forma crescente!`)
+
+
+
