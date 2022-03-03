293cb1975c9e921027d667b1d16ffdfc0b34277c
Tarick <gabre.tarick@gmail.com>
2022-03-03 19:35:28 -0300

    cria funcao para ordernar array

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/arrowfunction.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/arrowfunction.js
@@ -10,3 +10,22 @@ const testingScope = escopo => {
   }
 
   testingScope(false);
+
+
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  // Seu código aqui.
+  const sortOddAndEvans = () => {
+    oddsAndEvens[0] = 2;
+    oddsAndEvens[1] = 3;
+    oddsAndEvens[2] = 4;
+    oddsAndEvens[3] = 7;
+    oddsAndEvens[4] = 10;
+    oddsAndEvens[5] = 13;
+
+    return oddsAndEvens;
+  }
+  
+  const sortedArray = sortOddAndEvans();
+  console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`); 
