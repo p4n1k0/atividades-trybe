2a1a947aab187d0e0fd03d37aed0cbfd8cdc1515
Tarick <gabre.tarick@gmail.com>
2022-03-03 23:37:48 -0300

    funcao q ao clicar 2x em meu top 3... rediriciona para minha pagina web

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_3-javascript-eventos/main.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_3-javascript-eventos/main.js
@@ -32,7 +32,9 @@ input.addEventListener('input', function(event) {
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
-
+myWebpage.addEventListener('dblclick', function() {
+  window.location.replace('https://p4n1k0.github.io/');
+});
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
 
