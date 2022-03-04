0586b35271ed60f8859d37c442b27088d27eb7bb
Tarick <gabre.tarick@gmail.com>
2022-03-03 21:19:28 -0300

    funcao q acha maior palavra dentro e uma frase

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/parte2/script.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/parte2/script.js
@@ -1,2 +1,19 @@
 const fatorial = number => number > 1 ? number * fatorial(number - 1) : 1;
 console.log(fatorial(10));
+
+
+const longestWord = text => {
+    let wordArray = text.split(' ');
+    let maxLength = 0;
+    let result = '';
+
+    for (const word of wordArray) {
+        if (word.length > maxLength) {
+            maxLength = word.length;
+            result = word;
+        }
+    }
+    return result;
+}
+
+console.log(longestWord('Antonio foi no banheiro e não sabemos o que aconteceu'));
