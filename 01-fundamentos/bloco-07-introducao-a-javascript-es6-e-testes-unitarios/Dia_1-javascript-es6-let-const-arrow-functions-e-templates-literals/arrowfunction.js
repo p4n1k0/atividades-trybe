4aa34f5087548209ade5ee9da5a0b4d72e80cb2f
Tarick <gabre.tarick@gmail.com>
2022-03-05 12:27:16 -0300

    usa sort para ordenar array e template literals para imprimir msg

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_1-javascript-es6-let-const-arrow-functions-e-templates-literals/arrowfunction.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_1-javascript-es6-let-const-arrow-functions-e-templates-literals/arrowfunction.js
@@ -15,20 +15,14 @@ testingScope(true);
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
-const sortOddsAndEvens = () => {
-  oddsAndEvens[0] = 2;
-  oddsAndEvens[1] = 3;
-  oddsAndEvens[2] = 4;
-  oddsAndEvens[3] = 7;
-  oddsAndEvens[4] = 10;
-  oddsAndEvens[5] = 13;
-
-  return oddsAndEvens;
+const sortArrayBonus = array => {
+  const sortOddsAndEvens = array.sort((a, b) => a - b);
+  return sortOddsAndEvens;
 }
 
-const sortedArray = sortOddsAndEvens();
-console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
+const sortedArrayBonus = sortArrayBonus(oddsAndEvens);
+console.log(`Os números ${sortedArrayBonus} se encontram ordenados de forma crescente !`);
 
 
 
-console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente`); // será necessário alterar essa linha 😉
+
