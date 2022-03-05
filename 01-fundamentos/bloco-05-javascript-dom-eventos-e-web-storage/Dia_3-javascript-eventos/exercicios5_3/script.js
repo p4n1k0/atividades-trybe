a85af081c6e119f46e5f4083df62e8dd74e27f81
Tarick <gabre.tarick@gmail.com>
2022-03-05 18:59:59 -0300

    cria botao sexta-feira

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_3-javascript-eventos/exercicios5_3/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_3-javascript-eventos/exercicios5_3/script.js
@@ -77,3 +77,16 @@ function displayHolidays() {
 displayHolidays();
 
 
+
+function createFridayButton(buttonName) {
+  let buttonContainer = document.querySelector('.buttons-container');
+  let newButton = document.createElement('button');
+  let newButtonID = 'btn-friday';
+  newButton.innerHTML = buttonName;
+  newButton.id = newButtonID;
+  buttonContainer.appendChild(newButton);
+}
+
+createFridayButton('Sexta-feira');
+
+
