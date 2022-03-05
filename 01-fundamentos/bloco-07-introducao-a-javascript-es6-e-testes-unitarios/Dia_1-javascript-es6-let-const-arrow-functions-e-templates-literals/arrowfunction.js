00a1eed5801e7e94910116f6a26b1efa3abc5b47
Tarick <gabre.tarick@gmail.com>
2022-03-05 12:06:03 -0300

    ordena array de forma crescente e utiliza template literals

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_1-javascript-es6-let-const-arrow-functions-e-templates-literals/arrowfunction.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_1-javascript-es6-let-const-arrow-functions-e-templates-literals/arrowfunction.js
@@ -10,3 +10,18 @@ const testingScope = escopo => {
 }
 
 testingScope(true);
+
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+oddsAndEvens[0] = 2;
+oddsAndEvens[1] = 3;
+oddsAndEvens[2] = 4;
+oddsAndEvens[3] = 7;
+oddsAndEvens[4] = 10;
+oddsAndEvens[5] = 13;
+
+
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente`); // será necessário alterar essa linha 😉
