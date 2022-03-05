5560126481249f372f704f124d680d439be172da
Tarick <gabre.tarick@gmail.com>
2022-03-05 12:21:43 -0300

    corrige exercicio 1

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_1-javascript-es6-let-const-arrow-functions-e-templates-literals/arrowfunction.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_1-javascript-es6-let-const-arrow-functions-e-templates-literals/arrowfunction.js
@@ -15,12 +15,19 @@ testingScope(true);
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
-oddsAndEvens[0] = 2;
-oddsAndEvens[1] = 3;
-oddsAndEvens[2] = 4;
-oddsAndEvens[3] = 7;
-oddsAndEvens[4] = 10;
-oddsAndEvens[5] = 13;
+const sortOddsAndEvens = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+
+  return oddsAndEvens;
+}
+
+const sortedArray = sortOddsAndEvens();
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
 
 
 
