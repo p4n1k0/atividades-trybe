a471a9492329c471a50011aeafa1546417a5741c
Tarick <gabre.tarick@gmail.com>
2022-03-04 21:24:14 -0300

    cria dias de um calendario e coloca classe para sextas e feriados

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_3-javascript-eventos/exercicios5_3/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/Dia_3-javascript-eventos/exercicios5_3/script.js
@@ -1,194 +1,46 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
-  
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
-    };
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
   };
-  
-  createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
-
-  //ref: gabarito
-
-  let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-
-  function diasDezembro () {
-      let pegaDiasLista = document.querySelector('#days');
-
-      for (let i = 0; i < dezDaysList.length; i += 1) {
-          let day = dezDaysList[i];
-          let diaLista = document.createElement('li')
-          
-          if (day === 24 || day === 31) {
-            diaLista.className = 'day holiday';
-            diaLista.innerHTML = day;
-            pegaDiasLista.appendChild(diaLista);
-          } else if (day === 4 || day === 11 || day === 18) {
-            diaLista.className = 'day friday';
-            diaLista.innerHTML = day;
-            pegaDiasLista.appendChild(diaLista)
-          } else if (day === 25) {
-            diaLista.className = 'day holiday friday';
-            diaLista.innerHTML = day;
-            pegaDiasLista.appendChild(diaLista);
-          } else {
-            diaLista.innerHTML = day;
-            diaLista.className = 'day';
-            pegaDiasLista.appendChild(diaLista);
-          };
-      };
-  }
-
-  diasDezembro();
-
-  // ref: gabarito
-
-  function criaBotaoFeriado(nomeBotao) {
-    let containerBotao = document.querySelector('.buttons-container');
-    let novoBotao = document.createElement('button');
-    let novoBotaoId = 'btn-holiday';
-
-    novoBotao.innerHTML = nomeBotao;
-    novoBotao.id = novoBotaoId;
-    containerBotao.appendChild(novoBotao);
-  };
-
-criaBotaoFeriado('Feriados');
-
-// ref: gabarito
-function mostraFeriados() {
-  let pegaBotaoFeriado = document.querySelector('#btn-holiday');
-  let pegaFeriados = document.querySelectorAll('.holiday');
-  let bgColor = 'rgb(238,238,238)';
-  let achaNovaCor = 'white';
-
-  pegaBotaoFeriado.addEventListener('click', function() {
-    for (let i = 0; i < pegaFeriados.length; i += 1) {
-      if (pegaFeriados[i].style.backgroundColor === achaNovaCor) {
-        pegaFeriados[i].style.backgroundColor = bgColor;
-      } else {
-        pegaFeriados[i].style.backgroundColor = achaNovaCor;
-      }
-    }
-  })
-};    
-
-mostraFeriados();
-
-//4. ref: gabarito
-function criaBotaoSextaFeira(nomeBotao) {
-  let containerBotao = document.querySelector('.buttons-container');
-  let novoBotao = document.createElement('button');
-  let novoBotaoId = 'btn-friday';
-
-  novoBotao.innerHTML = nomeBotao;
-  novoBotao.id = novoBotaoId;
-  containerBotao.appendChild(novoBotao);
 };
 
-criaBotaoSextaFeira('sexta-feira');
+createDaysOfTheWeek();
 
-//5. ref: gabarito
-function mostraSextas(sextasArray) {
-  let pegaBotaoSextas = document.querySelector('#btn-friday');
-  let sextas = document.getElementsByClassName('friday');
-  let novoTextoSexta = 'Sextou!!';
+// Escreva seu código abaixo.
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-  pegaBotaoSextas.addEventListener('click', function(){
-    for (let i = 0; i < sextas.length; i += 1) {
-      if (sextas[i].innerHTML !== novoTextoSexta) {
-        sextas[i].innerHTML = novoTextoSexta;
-      } else {
-        sextas[i].innerHTML = sextasArray[i];
-      }
-    }
-  })  
-};
+function createDaysOfTheMonth() {
+  let getDaysList = document.querySelector('#days');
 
-let dezSextas = [4, 11, 18, 25];
-mostraSextas(dezSextas);
+  for (let i = 0; i < dezDaysList.length; i += 1) {
+    let day = dezDaysList[i];
+    let dayItem = document.createElement('li');
 
-//6. ref: gabarito
-function diaMouseOver() {
-  let dias = document.querySelector('#days');
-
-  dias.addEventListener('mouseover', function(event) {
-    event.target.style.fontSize = '30px';
-    event.target.style.fontWeight = '600';
-  }) 
-}
-
-function diaMouseOut() {
-  let dias = document.querySelector('#days');
-
-  dias.addEventListener('mouseout', function(event) {
-    event.target.style.fontWeight = '200';
-    event.target.style.fontSize = '20px';
-  })
-};
-
-diaMouseOver();
-diaMouseOut();
-
-//7. ref: gabarito
-function newTaskSpan(task) {
-  let tasksContainer = document.querySelector('.my-tasks');
-  let nomeTask = document.createElement('span');
-
-  nomeTask.innerHTML = task;
-  tasksContainer.appendChild(nomeTask);
-};
-
-newTaskSpan('Projeto');
-
-//8. ref: gabarito
-function novoTaskDiv(color) {
-  let tasksContainer = document.querySelector('.my-tasks');
-  let novoTask = document.createElement('div');
-
-  novoTask.className = 'task';
-  novoTask.style.backgroundColor = color;
-  tasksContainer.appendChild(novoTask);
-};
-
-novoTaskDiv('blue');
-
-//9. ref: gabarito
-function setTaskClass() {
-  let selecionaTask = document.getElementsByClassName('task selected');
-  let minhasTasks = document.querySelector('.task');
-
-  minhasTasks.addEventListener('click', function(event) {
-    if (selecionaTask.length === 0) {
-      event.target.className = 'task selected';
+    if (day === 24 || day === 31) {
+      dayItem.className = 'day holiday';
+      dayItem.innerHTML = day;
+      getDaysList.appendChild(dayItem);
+    } else if (day === 4 || day === 11 || day === 18) {
+      dayItem.className = 'day friday';
+      dayItem.innerHTML = day;
+      getDaysList.appendChild(dayItem);
+    } else if (day === 25) {
+      dayItem.className = 'day holiday friday';
+      dayItem.innerHTML = day;
+      getDaysList.appendChild(dayItem);
     } else {
-      event.target.className = 'task';
+      dayItem.innerHTML = day;
+      dayItem.className = 'day';
+      getDaysList.appendChild(dayItem);
     }
-  });
+  };  
 };
- 
-setTaskClass();
 
-function setDayColor() {
-  let selecionaTask = document.getElementsByClassName('task selected');
-  let dias = document.querySelector('#days');
-  let taskDiv = document.querySelector('.task');
-  let corTask = taskDiv.style.backgroundColor;
-
-  dias.addEventListener('click', function(event){
-    let eventoTargetCor = event.target.style.color;
-    if (selecionaTask.length > 0 && eventoTargetCor !== corTask) {
-      let cor = selecionaTask[0].style.backgroundColor;
-      event.target.style.color = cor;
-    } else if (eventoTargetCor === corTask && selecionaTask.length !== 0) {
-      event.target.style.color = 'rgb(119, 119, 119';
-    }
-  });
-};
+createDaysOfTheMonth();
---
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_2-javascript-es6-fluxo-de-excecao-e-objetos/scripts.js
@@ -0,0 +1,8 @@
+function addKey(customer, name, string) {
+    let newKey = 'name';
+    const nome = 'Gabriel';
+    customer[newKey] = name;
+    return customer;
+    console.log(customer);
+    
+}
