27ee4b84b34e0ab7570dda18d195194a58ecad45
Tarick <gabre.tarick@gmail.com>
2022-03-06 18:01:29 -0300

    arrow function q exibe nivel de habilidade

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_2-javascript-es6-fluxo-de-excecao-e-objetos/mains.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_2-javascript-es6-fluxo-de-excecao-e-objetos/mains.js
@@ -8,3 +8,32 @@ const objeto = (object, key, value) => {
 }
 
 console.log(objeto('customer', 'name', 'Gabriel'));
+
+
+const student1 = {
+    Html: 'Muito bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SofSkills: 'Ótimo'
+};
+
+const student2 = {
+    Html: 'Bom',
+    Css: 'Ótimo',
+    JavaScript: 'Ótimo',
+    Git: 'Bom',
+};
+
+const listSkills = student => {
+    const arrayOfSkills = Object.keys(student);
+
+    for (const index in arrayOfSkills) {
+        console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);        
+    }
+};
+
+console.log('Estudante 1');
+listSkills(student1);
+
+console.log('Estudante 2');
+listSkills(student2);
