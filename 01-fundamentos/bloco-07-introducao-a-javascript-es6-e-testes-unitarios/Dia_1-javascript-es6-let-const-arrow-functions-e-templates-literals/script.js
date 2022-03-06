9e9461cbf893ba95879a1bc39bb60985eb2afbfc
Tarick <gabre.tarick@gmail.com>
2022-03-06 00:14:03 -0300

    arrow function q acha maior palavra de um texto

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_1-javascript-es6-let-const-arrow-functions-e-templates-literals/script.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_1-javascript-es6-let-const-arrow-functions-e-templates-literals/script.js
@@ -17,4 +17,20 @@ console.log(fatorial(4));
 
 //Exercício 2 
 
+const longestWord = text => {
+    let wordArray = text.split(' ');
+    let maxLength = 0;
+    let result = '';
+
+    for (const word of wordArray) {
+        if (word.length > maxLength) {
+            maxLength = word.length;
+            result = word;
+        }        
+    }
+    return result;
+}
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
+
 
