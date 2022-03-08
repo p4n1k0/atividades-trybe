e614efc5ae3e2d10477d195169468960aff41acb
Tarick <gabre.tarick@gmail.com>
2022-03-07 22:52:22 -0300

    chamada com um numero divisivel por 3 e verifica se retorno e esperado

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_3-primeiros-passos-em-jest/myFizzBuzz.test.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_3-primeiros-passos-em-jest/myFizzBuzz.test.js
@@ -1,8 +1,10 @@
+const { expect } = require('@jest/globals');
 const myFizzBuzz = require('./myFizzBuzz.js');
 
 describe('testa a função myFizzBuzz', () => {
     it('verifica, se de acordo com o parâmetro recebido, retorna o valor esperado', () => {
         expect(myFizzBuzz(15)).toBe('fizzbuzz');
+        expect(myFizzBuzz(9)).toBe('fizz');
     }) 
 })
 
