55e108412f906a4c7cd78f9809e925ea6b0e59fc
Tarick <gabre.tarick@gmail.com>
2022-03-08 01:18:50 -0300

    verifica se funcao encode converte letras em numeros

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_3-primeiros-passos-em-jest/encodeDecode.test.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_3-primeiros-passos-em-jest/encodeDecode.test.js
@@ -8,7 +8,19 @@ describe('Testa a função encode e decode', () => {
         expect(typeof encode).toEqual('function');
     });
     it('converte apenas a vogal "a" no número 1', () => {
-      expect(encode('ana')).toEqual('1n1')
+      expect(encode('ana')).toEqual('1n1');
+    });
+    it('converte apenas a vogal "e" no número 2', () => {
+      expect(encode('ele')).toEqual('2l2');
+    });
+    it('converte apenas a vogal "i" no número 3', () => {
+      expect(encode('filipi')).toEqual('f3l3p3');
+    });
+    it('converte apenas a vogal "o" no número 4', () => {
+      expect(encode('ovo')).toEqual('4v4');
+    });
+    it('converte apenas a vogal "u" no número 5', () => {
+      expect(encode('urubu')).toEqual('5r5b5');
     });
     
     it('a função decode é definida', () => {
