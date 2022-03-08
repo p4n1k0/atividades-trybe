564c53012b2fe713b9beb998bb439b2bd939b8d3
Tarick <gabre.tarick@gmail.com>
2022-03-07 21:27:48 -0300

    retorna erro caso receba como parametro uma string

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_3-primeiros-passos-em-jest/sum.test.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_3-primeiros-passos-em-jest/sum.test.js
@@ -2,6 +2,10 @@ const sum = require('./sum');
 
 describe('testa a função sum', () => {
     it('deve retornar o resultado da soma', () => {
-        expect(sum(4, 5)).toBe(9);        
-    });    
+        expect(sum(4, 5)).toBe(9); 
+        expect(sum(0, 0)).toBe(0);       
+    });
+    it('deve retornar um erro caso receba como parâmetro uma string', () => {
+        expect(() => sum(4, '5')).toThrowError();
+    })    
 })
