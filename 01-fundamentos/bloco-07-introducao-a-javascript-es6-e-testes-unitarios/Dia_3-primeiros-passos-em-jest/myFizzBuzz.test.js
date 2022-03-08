79335e0632979d35e59740b74ee3718447913a9b
Tarick <gabre.tarick@gmail.com>
2022-03-07 23:04:16 -0300

    chamada com numero divisivel por 5 e verifica seu retorno esperado

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_3-primeiros-passos-em-jest/myFizzBuzz.test.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_3-primeiros-passos-em-jest/myFizzBuzz.test.js
@@ -5,6 +5,7 @@ describe('testa a função myFizzBuzz', () => {
     it('verifica, se de acordo com o parâmetro recebido, retorna o valor esperado', () => {
         expect(myFizzBuzz(15)).toBe('fizzbuzz');
         expect(myFizzBuzz(9)).toBe('fizz');
+        expect(myFizzBuzz(10)).toBe('buzz');
     }) 
 })
 
