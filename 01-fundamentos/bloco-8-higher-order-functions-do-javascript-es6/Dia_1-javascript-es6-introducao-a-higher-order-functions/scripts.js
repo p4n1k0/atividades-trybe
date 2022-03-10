a37116659cfb754bdbdba0ee7791ae08d48c623d
Tarick <gabre.tarick@gmail.com>
2022-03-09 23:07:59 -0300

    exercise 2

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_1-javascript-es6-introducao-a-higher-order-functions/scripts.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_1-javascript-es6-introducao-a-higher-order-functions/scripts.js
@@ -6,7 +6,7 @@ const employeeGenerator = fullname => {
     };
 };
 
-const newEmployees = (callback) => {
+const newEmployees = callback => {
     const employees = {
       id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
       id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
@@ -18,3 +18,12 @@ const newEmployees = (callback) => {
 console.log(newEmployees(employeeGenerator));
 
 
+const numberChecker = (myNumber, number) => myNumber === number;
+
+const lotteryResult = (myNumber, callback) => {
+    const number = Math.floor((Math.random() * 5) + 1);
+    
+    return callback(myNumber, number) ? `Lucky day, you won!` : `Try Again!`;
+};
+
+console.log(lotteryResult(2, numberChecker));
