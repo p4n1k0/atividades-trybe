e4ad848b94eb8cc50d72d35be9fa2d1caeaee59f
Tarick <gabre.tarick@gmail.com>
2022-03-10 15:42:14 -0300

    usa find para encontrar primeiro nome com 5 letras

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
@@ -23,4 +23,13 @@ const findDivisibleBy3And5 = () => {
 }
 
 console.log(findDivisibleBy3And5());
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  // Adicione seu código aqui:
+  return names.find((name) => name.length === 5);
+}
+
+console.log(findNameWithFiveLetters());
   
