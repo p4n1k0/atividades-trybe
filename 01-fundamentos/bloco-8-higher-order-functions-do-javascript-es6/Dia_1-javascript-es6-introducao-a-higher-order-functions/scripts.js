7064346690780ff9063394b4df67623fb8b4470b
Tarick <gabre.tarick@gmail.com>
2022-03-10 00:17:35 -0300

    refatora exercicio 1 para melhor entendimento

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_1-javascript-es6-introducao-a-higher-order-functions/scripts.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_1-javascript-es6-introducao-a-higher-order-functions/scripts.js
@@ -1,29 +1,34 @@
-const employeeGenerator = fullname => {
+function employeeGenerator (fullname) {
     const email = fullname.toLowerCase().replace(/ /g, '_');
+    
     return {
-        fullname, 
-        email: `${email}@trybe.com` 
+        fullname,
+        email: `${email}@trybe.com`
     };
 };
 
-const newEmployees = callback => {
+function newEmployees(callback) {
     const employees = {
       id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
       id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
       id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-    };
+    }
     return employees;
-};
+  };
 
-console.log(newEmployees(employeeGenerator));
+  console.log(newEmployees(employeeGenerator)); 
 
 
-const numberChecker = (myNumber, number) => myNumber === number;
+
+function numberChecker (myNumber, number) {
+    if (myNumber === number) {
+        return 'Lucky day, you won!';
+    }
+    if (myNumber !== number) {
+        return 'Try Again!';
+    }
+}
 
 const lotteryResult = (myNumber, callback) => {
-    const number = Math.floor((Math.random() * 5) + 1);
-    
-    return callback(myNumber, number) ? `Lucky day, you won!` : `Try Again!`;
-};
 
-console.log(lotteryResult(2, numberChecker));
+}
