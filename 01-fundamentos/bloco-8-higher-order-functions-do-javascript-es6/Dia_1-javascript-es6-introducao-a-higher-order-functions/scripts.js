67523f1a2b65d95a469d973323b8174754e9b2f4
Tarick <gabre.tarick@gmail.com>
2022-03-10 01:16:54 -0300

    exercicio 3

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_1-javascript-es6-introducao-a-higher-order-functions/scripts.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_1-javascript-es6-introducao-a-higher-order-functions/scripts.js
@@ -35,3 +35,27 @@ function lotteryResult (myNumber, callback) {
 }
 
 console.log(lotteryResult(2, numberChecker));
+
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+function compareAnswers (rigthAnswer, studentAnswer) {
+    if (rigthAnswer === studentAnswer) {
+        return 1;
+    }
+    if (studentAnswer === 'N.A') {
+        return 0;
+    }
+    return -0.5;
+};
+
+function countPoints (rigthAnswers, studentAnswers, callback) {
+    let count = 0;
+    for (let i = 0; i < rigthAnswers.length; i += 1) {
+        const callbackReturn = callback(rigthAnswers[i], studentAnswers[i]);
+        count = count + callbackReturn;
+    }
+    return `Resultado final: ${count} pontos`;
+};
+
+console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
