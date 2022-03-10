51b7e43703808f3e6c1eed5ff5b148cd73e7252c
Tarick <gabre.tarick@gmail.com>
2022-03-10 15:34:09 -0300

    usa find para retornar o primeiro numero do array divisivel por 3 e 5

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
@@ -13,4 +13,14 @@ const emailListInData = [
 
   emailListInData.forEach(showEmailList);
   console.log(emailListInData);
+
+
+  const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  // Adiciona seu código aqui
+  return numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+}
+
+console.log(findDivisibleBy3And5());
   
