86cbfdf5168b2d2b219062581452d5e3df9f1529
Tarick <gabre.tarick@gmail.com>
2022-03-11 20:56:40 -0300

    exercise 4

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_3-javascript-es6-higher-order-functions-map-e-filter/pratica/exercise4.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_3-javascript-es6-higher-order-functions-map-e-filter/pratica/exercise4.js
@@ -62,38 +62,34 @@ const books = [
   ];
   
   const expectedResult = [
-    { 
-      id: 1,
-      name: 'As Crônicas de Gelo e Fogo',
-      genre: 'Fantasia',
-      author: { name: 'George R. R. Martin', birthYear: 1948 },
-      releaseYear: 1991
-    },
     {
-      id: 2,
-      name: 'O Senhor dos Anéis',
-      genre: 'Fantasia',
-      author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-      releaseYear: 1954
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+      releaseYear: 1928,
     },
     {
       id: 3,
       name: 'Fundação',
       genre: 'Ficção Científica',
       author: { name: 'Isaac Asimov', birthYear: 1920 },
-      releaseYear: 1951
+      releaseYear: 1951,
     },
     {
-      id: 4,
-      name: 'Duna',
-      genre: 'Ficção Científica',
-      author: { name: 'Frank Herbert', birthYear: 1920 },
-      releaseYear: 1965
-    }
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+      releaseYear: 1954,
+    },
   ];
-  
-  function fantasyOrScienceFiction() {
-    return books.filter((book) => (book.genre === 'Fantasia' || book.genre === 'Ficção Científica'));
+  //ref: gabarito
+  function oldBooksOrdered() {
+    const currentYear = new Date().getFullYear();
+    return books.filter((book) => (
+      book.releaseYear < currentYear - 60
+    )).sort((a, b) => a.releaseYear - b.releaseYear);
   }
-
-  console.log(fantasyOrScienceFiction());
+  
+  console.log(oldBooksOrdered());
