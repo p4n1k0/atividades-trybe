049c7d745024923f0c3d4eddbf8554d68ef2e500
Tarick <gabre.tarick@gmail.com>
2022-03-14 22:52:11 -0300

    exercise 5

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_3-javascript-es6-higher-order-functions-map-e-filter/pratica/exercise5.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_3-javascript-es6-higher-order-functions-map-e-filter/pratica/exercise5.js
@@ -69,8 +69,10 @@ const books = [
   ];
  
   function fantasyOrScienceFictionAuthors() {
-
-  }
-
+    const wantedGenres = ['Fantasia', 'Ficção Científica'];
+    return books.filter((book) => wantedGenres.includes(book.genre))
+    .map((book) => book.author.name).sort();
+  } 
+   
   console.log(fantasyOrScienceFictionAuthors());
 
