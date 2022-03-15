f1db356e32f4b0651cd0a6fe4cdf9f0d29838f31
Tarick <gabre.tarick@gmail.com>
2022-03-14 23:39:07 -0300

    exercise 7

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_3-javascript-es6-higher-order-functions-map-e-filter/pratica/exercise7.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_3-javascript-es6-higher-order-functions-map-e-filter/pratica/exercise7.js
@@ -64,8 +64,13 @@ const books = [
   const expectedResult = 'O Senhor dos Anéis';
 
   function authorWith3DotsOnName() {
-    
+    return books.filter((book) => (
+      book.author.name[1] === '.'
+      && book.author.name[4] === '.'
+      && book.author.name[7] === '.'
+    ))[0].name;
   }
+    
 
   console.log(authorWith3DotsOnName());
         
