850b265a6888f028c2d677fd14c53abcd44b4191
Tarick <gabre.tarick@gmail.com>
2022-03-14 21:52:37 -0300

    exercise 1

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_1-javascript-es6-introducao-a-higher-order-functions/pratica.js/exercise1.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_1-javascript-es6-introducao-a-higher-order-functions/pratica.js/exercise1.js
@@ -1,12 +1,18 @@
-
+const employeeGenerator = (fullName) => {  
+  const email = fullName.toLowerCase().replace(/ /g, '_');
+  return {
+    fullName,
+    email: `${email}@trybe.com`
+  };
+}
 
 const newEmployees = (callback) => {
     const employees = {
-      id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: '' // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+      id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
     return employees;
 };
 
-
+console.log(newEmployees(employeeGenerator));
