5b270eb109cfafb94623003d422818f0478c383d
Tarick <gabre.tarick@gmail.com>
2022-03-15 22:05:48 -0300

    test default destructuring

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixar.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixar.js
@@ -27,23 +27,23 @@ const weekdays = [...workDays, ...weekend];
 console.log(weekdays); // ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 
-const user = {
-  name: 'Maria',
-  age: 21,
-  nationality: 'Brazilian',
-};
+// const user = {
+//   name: 'Maria',
+//   age: 21,
+//   nationality: 'Brazilian',
+// };
 
-const jobInfos = {
-  profession: 'Software engineer',
-  squad: 'Rocket Landing Logic',
-  squadInitials: 'RLL',
-};
+// const jobInfos = {
+//   profession: 'Software engineer',
+//   squad: 'Rocket Landing Logic',
+//   squadInitials: 'RLL',
+// };
 
-const customer = { ...user, ...jobInfos };
+// const customer = { ...user, ...jobInfos };
 
-const { name, age, nationality, profession, squad, squadInitials } = customer;
+// const { name, age, nationality, profession, squad, squadInitials } = customer;
 
-console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);
+// console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);
 
 
 
@@ -62,3 +62,20 @@ let numerosPares = [1, 3, 5, 6, 8, 10, 12];
 console.log(numerosPares); // [6, 8, 10, 12];
 
 // Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+
+const getNationality = ({ firstName, nationality = 'brazilian' }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
