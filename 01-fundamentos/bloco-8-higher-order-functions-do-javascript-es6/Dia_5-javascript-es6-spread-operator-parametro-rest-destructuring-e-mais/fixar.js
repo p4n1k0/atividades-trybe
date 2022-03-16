f2f7b333408ab005d51d4cf331ba715c6f5f5768
Tarick <gabre.tarick@gmail.com>
2022-03-15 21:57:44 -0300

    test array destructuring for get numbers even

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixar.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixar.js
@@ -54,3 +54,11 @@ let bebida = 'arroz';
 [comida, animal, bebida] = [bebida, comida, animal];
 
 console.log(comida, animal, bebida); // arroz gato água
+
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+[,,, ...numerosPares] = numerosPares;
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
