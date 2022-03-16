15ef014ffb6151db30c3253bced41383d4364312
Tarick <gabre.tarick@gmail.com>
2022-03-15 21:03:48 -0300

    destructuing object and use template litelas for phrase

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixar.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/Dia_5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixar.js
@@ -41,4 +41,6 @@ const jobInfos = {
 
 const customer = { ...user, ...jobInfos };
 
-console.log(customer);
+const { name, age, nationality, profession, squad, squadInitials } = customer;
+
+console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);
