b25801d722ffec83416b44ba2e6d10f21ff39f79
Tarick <gabre.tarick@gmail.com>
2022-05-06 15:07:52 -0300

    exercice 5

--- 02-desenvolvimento-front-end/bloco-14-testes-automotizados-com-react-testing-library/Dia_02-rtl-mocks-e-inputs/exercices/src/service.js
+++ 02-desenvolvimento-front-end/bloco-14-testes-automotizados-com-react-testing-library/Dia_02-rtl-mocks-e-inputs/exercices/src/service.js
@@ -3,4 +3,11 @@ const firstFunction = (str) => str.toUpperCase();
 const secondFunction = (str) => str.CharAt(0);
 const thirdFunction = (str1,str2) => str1.concat(str2);
 
-module.exports = { randomNumber, firstFunction, secondFunction, thirdFunction };
+function fetchDog() {
+    return fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json()
+    .then(json => response.ok ? Promise.resolve(json) : Promise.reject(json)));
+};
+
+module.exports = { randomNumber, firstFunction, secondFunction, thirdFunction,
+fetchDog };
