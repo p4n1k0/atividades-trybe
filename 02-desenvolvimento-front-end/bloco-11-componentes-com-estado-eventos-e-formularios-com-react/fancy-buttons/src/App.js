0452237d050620864c75d3f32c9d809ad38f25c8
Tarick <gabre.tarick@gmail.com>
2022-05-31 20:54:38 -0300

    cria botoes e imprime msgs diferentes

--- 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/fancy-buttons/src/App.js
+++ 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/fancy-buttons/src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
-function handleClick() {
-  console.log('Clicou no botao');
+function handleButtonOne() {
+  console.log('Clicou no botao 1!');
 }
+
+function handleButtonTwo() {
+  console.log('Clicou no botao 2!');
+}
+
+function handleButtonThree() {
+  console.log('Clicou no botao 3!');
+}
+
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <button onClick={handleClick}>My button</button>
+        <button onClick={handleButtonOne}>button one</button>
+        <button onClick={handleButtonTwo}>button two2</button>
+        <button onClick={handleButtonThree}>button three</button>
       </div>
     );
   }  
