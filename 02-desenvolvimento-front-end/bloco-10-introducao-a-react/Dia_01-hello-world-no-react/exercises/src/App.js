c2e75e3501d92cf5487fc5ffde029177f3f05cc6
Tarick <gabre.tarick@gmail.com>
2022-05-31 11:22:05 -0300

    completa o codigo e coloca produtos como filhos de app

--- 02-desenvolvimento-front-end/bloco-10-introducao-a-react/Dia_01-hello-world-no-react/exercises/src/App.js
+++ 02-desenvolvimento-front-end/bloco-10-introducao-a-react/Dia_01-hello-world-no-react/exercises/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 //aproveita função do portfolio
-const Task = (value) => { return (<li key={value}>{value}</li>) }
+const Task = (value) => { return (<li key={value}>{value}</li>) };
 
 const tarefas = ['Acordar', 'Tomar-café', 'Estudar', 'Almoçar'];
 
