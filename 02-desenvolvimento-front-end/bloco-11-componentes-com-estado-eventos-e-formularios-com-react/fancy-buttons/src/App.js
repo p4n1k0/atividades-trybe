53b896b867a5a06456fe22ea5d670326abea697f
Tarick <gabre.tarick@gmail.com>
2022-05-31 21:18:07 -0300

    declara dentro da classe as funcoes que lidam com evento

--- 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/fancy-buttons/src/App.js
+++ 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/fancy-buttons/src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 
-function handleButtonOne() {
-  console.log('Clicou no botao 1!');
-}
-
-function handleButtonTwo() {
-  console.log('Clicou no botao 2!');
-}
-
-function handleButtonThree() {
-  console.log('Clicou no botao 3!');
-}
-
-
 export default class App extends Component {
+  handleButtonOne() {
+    console.log('Clicou no botao 1!');
+  }
+
+  handleButtonTwo() {
+    console.log('Clicou no botao 2!');
+  }
+
+  handleButtonThree() {
+    console.log('Clicou no botao 3!');
+  }
   render() {
     return (
       <div className="App">
-        <button onClick={handleButtonOne}>button one</button>
-        <button onClick={handleButtonTwo}>button two2</button>
-        <button onClick={handleButtonThree}>button three</button>
+        <button onClick={this.handleButtonOne}>button one</button>
+        <button onClick={this.handleButtonTwo}>button two2</button>
+        <button onClick={this.handleButtonThree}>button three</button>
       </div>
     );
-  }  
+  }
 }
