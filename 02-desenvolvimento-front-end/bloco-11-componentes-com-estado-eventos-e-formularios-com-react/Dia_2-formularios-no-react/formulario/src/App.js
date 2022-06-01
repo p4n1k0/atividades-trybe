172938438b6f27446e3ccf38c9df9e367fc23531
Tarick <gabre.tarick@gmail.com>
2022-06-01 19:55:00 -0300

    encapsula campos num fieldset

--- 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/Dia_2-formularios-no-react/formulario/src/App.js
+++ 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/Dia_2-formularios-no-react/formulario/src/App.js
@@ -17,80 +17,93 @@ class App extends Component {
   handleChange({ target }) {
     const { name } = target;
     const value = (target.type === 'checkbox') ? target.checked : target.value;
-    
+
     this.setState({
       [name]: value,
     });
   }
 
+  renderSelectInput() {
+    const { age } = this.state;
+
+    return (
+      <label htmlFor="age">
+        Idade:
+        <select
+          id="age"
+          name="age"
+          onChange={this.handleChange}
+          value={age}
+        >
+          <option value="">Selecione</option>
+          <option value="adult">Maior que 18</option>
+          <option value="underage">Menor que 18</option>
+        </select>
+      </label>
+    )
+  }
+
   render() {
-    const { name, email, age, anecdote, terms } = this.state;
+    const { name, email, anecdote, terms } = this.state;
 
     return (
       <div>
         <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
         <form className="form">
+          <fieldset>
+            <legend>Informações pessoais</legend>
 
-          <label htmlFor="name">
-            Nome:
-            <input
-              id="name"
-              name="name"
-              type="text"
-              onChange={ this.handleChange }
-              value={ name }
-            />
-          </label>
+            <label htmlFor="name">
+              Nome:
+              <input
+                id="name"
+                name="name"
+                type="text"
+                onChange={this.handleChange}
+                value={name}
+              />
+            </label>
 
-          <label htmlFor="email">
-            Email:
-            <input
-              id="email"
-              name="email"
-              type="email"
-              onChange={ this.handleChange }
-              value={ email }
-            />
-          </label>
+            <label htmlFor="email">
+              Email:
+              <input
+                id="email"
+                name="email"
+                type="email"
+                onChange={this.handleChange}
+                value={email}
+              />
+            </label>
 
-          <label htmlFor="age">
-            Idade:
-            <select
-              id="age"
-              name="age"
-              onChange={ this.handleChange }
-              value={ age }
-            >
-              <option value="">Selecione</option>
-              <option value="adult">Maior que 18</option>
-              <option value="underage">Menor que 18</option>
-            </select>
-          </label>
+            {this.renderSelectInput()}
+          </fieldset>
 
-          <label htmlFor="anecdote">
-            Anedota:
-            <textarea 
-              id="anecdote"
-              name="anecdote"
-              onChange={ this.handleChange }
-              value={ anecdote }
-            />
-          </label>
+          <fieldset>
+            <label htmlFor="anecdote">
+              Anedota:
+              <textarea
+                id="anecdote"
+                name="anecdote"
+                onChange={this.handleChange}
+                value={anecdote}
+              />
+            </label>
+
+            <input type="file" />
+          </fieldset>
 
           <label htmlFor="terms">
             <input
               id="terms"
               type="checkbox"
               name="terms"
-              onChange={ this.handleChange }
-              value={ terms }
+              onChange={this.handleChange}
+              value={terms}
             />
             Concordo com termos e acordos
           </label>
-
-          <input type="file" />
         </form>
-      </div>
+      </div >
     );
   }
 }
