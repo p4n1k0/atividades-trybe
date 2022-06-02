0c47405c84d5471f8ae0ce9fd99f06e09c4b2d9d
Tarick <gabre.tarick@gmail.com>
2022-06-01 21:13:45 -0300

    duas validacoes em comp filho e uma em outro comp filho

--- 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/Dia_2-formularios-no-react/formulario/src/DataFieldset.js
+++ 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/Dia_2-formularios-no-react/formulario/src/DataFieldset.js
@@ -1,30 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export default class DataFieldset extends Component {
-    render() {
-        const { anecdoteValue, handleChange } = this.props;
+class DataFieldset extends Component {
+  render() {
+    const { anecdoteValue, handleChange } = this.props;
 
-        return (
-            <fieldset>
-                <legend>Texto e arquivos</legend>
-                <label htmlFor="anecdote">
-                    Anedota:
-                    <textarea
-                      id="anecdote"
-                      name="anecdote"
-                      onChange={ handleChange }
-                      value={ anecdoteValue }
-                    />
-                </label>
-
-                <input type="file" />
-            </fieldset>
-        )
-    }
+    return (
+      <fieldset>
+        <legend>Texto e arquivos</legend>
+        <label htmlFor="anecdote">
+          Anedota:
+          <textarea
+            id="anecdote"
+            name="anecdote"
+            onChange={ handleChange }
+            value={ anecdoteValue }
+          />
+          { !anecdoteValue.length ? ' -campo vazio- ' : ' -ok- '}
+        </label>
+        <input type="file" />
+      </fieldset>
+    );
+  }
 }
 
 DataFieldset.propTypes = {
-    handleChange: PropTypes.func,
-    anecdoteValue: PropTypes.string,
-}.isRequired
+  handleChange: PropTypes.func.isRequired,
+  anecdoteValue: PropTypes.string.isRequired,
+};
+
+export default DataFieldset;
