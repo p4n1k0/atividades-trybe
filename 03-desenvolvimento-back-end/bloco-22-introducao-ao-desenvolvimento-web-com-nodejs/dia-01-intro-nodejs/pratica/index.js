4b31e1354e173f1c7edcd593edc427e56a7d3102
Tarick <gabre.tarick@gmail.com>
2022-08-19 14:33:57 -0300

    exportando funcao de conversao e valor do dolar

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-intro-nodejs/pratica/index.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-intro-nodejs/pratica/index.js
@@ -1,6 +1,6 @@
-const convert = require('./brlValue');
+const brlValue = require('./brlValue');
 
-const usd = 10;
-const brl = convert(usd);
+console.log(brlValue); // { brl: 5.37, usdToBrl: [Function: usdToBrl] }
 
-console.log(brl) 
+console.log(`Valor do dólar: ${brlValue.brl}`); // Valor do dólar: 5.37
+console.log(`10 dólares em reais: ${brlValue.usdToBrl(10)}`); // 10 dólares em reais: 53.7
