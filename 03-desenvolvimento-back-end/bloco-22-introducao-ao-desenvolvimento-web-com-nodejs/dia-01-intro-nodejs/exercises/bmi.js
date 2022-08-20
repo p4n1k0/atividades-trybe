960cb06eaa537b554fef75e6642b0c47a8e81b4c
Tarick <gabre.tarick@gmail.com>
2022-08-20 20:31:08 -0300

    calculo do IMC

---
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-intro-nodejs/exercises/bmi.js
@@ -0,0 +1,22 @@
+const weightInKg = 70; 
+const heightInCm = 174; 
+
+function handleBMI(weight, height) {
+  console.log(`Weight: ${weight}, Height: ${height}`);
+
+  const heightInMeters = height / 100;
+  const heightSquared = heightInMeters ** 2;
+
+  const bmi = height / heightSquared;
+  
+  return bmi;
+}
+
+// A função main é o ponto de partida do nosso programa
+function main() {
+  const bmi = handleBMI(weightInKg, heightInCm);
+
+  console.log(`BMI: ${bmi.toFixed(2)}`);
+}
+
+main();
