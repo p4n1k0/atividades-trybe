78a59256a694f222f4c700de3382eb95b94b94a5
Tarick <gabre.tarick@gmail.com>
2022-08-20 21:05:51 -0300

    funcao p realizar leitura do arquivo e conversao do JSON em obj

---
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-intro-nodejs/exercises/simpsom.js
@@ -0,0 +1,8 @@
+const fs = require('fs').promises;
+
+async function getSimpsonById(id) {
+  const fileContent = await fs
+    .readFile('./simpsons.json', 'utf-8');
+  
+  const simpsons = JSON.parse(fileContent);
+}
