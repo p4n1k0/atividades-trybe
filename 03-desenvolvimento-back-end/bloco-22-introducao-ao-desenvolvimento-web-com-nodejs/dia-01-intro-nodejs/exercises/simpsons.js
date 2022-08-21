3d36d95e7bce91165adb39608b68eac7df2c29c7
Tarick <gabre.tarick@gmail.com>
2022-08-20 21:00:44 -0300

    converte conteudo json para array usando JSON.parse

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-intro-nodejs/exercises/simpsons.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-intro-nodejs/exercises/simpsons.js
@@ -2,6 +2,7 @@ const fs = require('fs').promises;
 
 async function readAll() {
     const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
+    const simpsons = JSON.parse(fileContent);
   }  
 
   async function main() {
