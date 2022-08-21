ecb6957a2a34ef1443b1e808202e7ff2609ea4a6
Tarick <gabre.tarick@gmail.com>
2022-08-20 21:06:50 -0300

    busca simpsom desejado e dispara erro caso n encontra

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-intro-nodejs/exercises/simpsom.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-intro-nodejs/exercises/simpsom.js
@@ -5,4 +5,10 @@ async function getSimpsonById(id) {
     .readFile('./simpsons.json', 'utf-8');
   
   const simpsons = JSON.parse(fileContent);
+
+  const chosenSimpson = simpsons.find((simpson) => Number(simpson.id) === id);
+
+     if (!chosenSimpson) {
+       throw new Error('id não encontrado');
+     }
 }
