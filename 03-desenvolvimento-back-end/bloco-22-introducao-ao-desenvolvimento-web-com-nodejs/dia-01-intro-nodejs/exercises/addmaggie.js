602a972765205aa2ecf2be40ada1ab06e0262bbe
Tarick <gabre.tarick@gmail.com>
2022-08-22 13:21:04 -0300

    cria funcao q substitui personagem por outro, filter p achar id e add maggie

---
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-intro-nodejs/exercises/addmaggie.js
@@ -0,0 +1,11 @@
+async function replaceNelson() {
+    const fileContent = await fs.readFile('./simpsonsFamily.json', 'utf-8');
+    const simpsons = JSON.parse(fileContent);
+  
+    const simpsonsWithoutNelson = simpsons.filter((simpson) => simpson.id !== '8');
+  
+    const simpsonsWithMaggie = simpsonsWithoutNelson
+      .concat([{ id: '15', name: 'Maggie Simpson' }]);
+  
+    return fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsWithMaggie));
+  }
