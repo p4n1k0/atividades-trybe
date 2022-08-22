9ce2ebc12488c2ff15563de0bbcbc28c61ae566b
Tarick <gabre.tarick@gmail.com>
2022-08-22 18:17:23 -0300

    cria array Corinthians e bambys

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/soccer-team-manager/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/soccer-team-manager/src/app.js
@@ -1,5 +1,20 @@
 const express = require('express');
 
+const teams = [
+    {
+        id: 1,
+        name: 'Sport Club Corinthians Paulista',
+        initials: 'COR',
+    },
+    {
+        id: 2,
+        name: 'São Paulo Futebol Clube',
+        initials: 'SPF',
+    },
+];
+
 const app = express();
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
 
 module.exports = app;
