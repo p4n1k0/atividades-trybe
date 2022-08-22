1841c7b5d2990a2bd60fc478c4f956fa681bf6e0
Tarick <gabre.tarick@gmail.com>
2022-08-22 18:31:10 -0300

    lista times pelo metodo get

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/soccer-team-manager/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/soccer-team-manager/src/app.js
@@ -15,8 +15,17 @@ const teams = [
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
 
 app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
 
 module.exports = app;
