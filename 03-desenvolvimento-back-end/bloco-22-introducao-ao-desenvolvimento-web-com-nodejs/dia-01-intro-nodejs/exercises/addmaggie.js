b04cd46e1910c195d092362629f27b96c196e50a
Tarick <gabre.tarick@gmail.com>
2022-08-22 13:28:35 -0300

    faz troca de simpsons no arqvo JSON

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-intro-nodejs/exercises/addmaggie.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-intro-nodejs/exercises/addmaggie.js
@@ -1,11 +1,19 @@
+const fs = require('fs').promises;
+
 async function replaceNelson() {
     const fileContent = await fs.readFile('./simpsonsFamily.json', 'utf-8');
     const simpsons = JSON.parse(fileContent);
   
     const simpsonsWithoutNelson = simpsons.filter((simpson) => simpson.id !== '8');
   
-    const simpsonsWithMaggie = simpsonsWithoutNelson
-      .concat([{ id: '15', name: 'Maggie Simpson' }]);
+    const simpsonsWithMaggie = simpsonsWithoutNelson.concat([{ id: '15', name: 'Maggie Simpson' }]);
+    console.log(simpsonsWithMaggie);
   
     return fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsWithMaggie));
   }
+
+  async function main() {
+    await replaceNelson(); 
+  }
+
+  main()
