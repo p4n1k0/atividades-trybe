773e8a7eba13872fde07f9f629278b95e167fc18
Tarick <gabre.tarick@gmail.com>
2022-08-22 12:48:49 -0300

    altera arquivo simpsons.json retirando ids 10 e 6

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-intro-nodejs/exercises/rmsimpsons.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-intro-nodejs/exercises/rmsimpsons.js
@@ -3,10 +3,18 @@ const fs = require('fs').promises;
 async function filterSimpsons() {
   const fileContent = await fs
     .readFile('./simpsons.json', 'utf-8');
-  
+
   const simpsons = JSON.parse(fileContent);
 
   const newArray = simpsons.filter((simpson) => simpson.id !== '10' && simpson.id !== '6');
+  console.log(newArray);
 
   await fs.writeFile('./simpsons.json', JSON.stringify(newArray));
+
 }
+
+async function main() {
+   await filterSimpsons();  
+}
+
+main();
