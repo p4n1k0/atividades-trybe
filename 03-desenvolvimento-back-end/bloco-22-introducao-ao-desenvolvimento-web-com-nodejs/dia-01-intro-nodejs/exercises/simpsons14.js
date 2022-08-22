027d7026615daf9de69154bae351b1919f50ad49
Tarick <gabre.tarick@gmail.com>
2022-08-22 12:55:09 -0300

    funcao para leitura do arqvo e o parse do JSON

---
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-intro-nodejs/exercises/simpsons14.js
@@ -0,0 +1,9 @@
+const fs = require('fs').promises;
+
+async function createSimpsonsFamily() {
+  const fileContent = await fs
+    .readFile('./simpsons.json', 'utf-8');
+
+  const simpsons = JSON.parse(fileContent);
+}
+// ...
