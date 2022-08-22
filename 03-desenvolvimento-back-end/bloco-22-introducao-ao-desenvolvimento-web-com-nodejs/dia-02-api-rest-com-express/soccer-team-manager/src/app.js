abb082c0281e7b9722f0f6758811856162131147
Tarick <gabre.tarick@gmail.com>
2022-08-22 18:49:23 -0300

    edita times pelo metodo put

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/soccer-team-manager/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/soccer-team-manager/src/app.js
@@ -21,11 +21,19 @@ app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
 
 app.get('/teams', (req, res) => res.status(200).json({ teams }));
 
-app.post('/teams', (req, res) => {
-  const newTeam = { ...req.body };
-  teams.push(newTeam);
-
-  res.status(201).json({ team: newTeam });
+app.put('/teams/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, initials } = req.body;
+    let updatedTeam;
+    for (let i = 0; i < teams.length; i += 1) {
+        const team = teams[i];
+        if (team.id === Number(id)) {
+            team.name = name;
+            team.initials = initials;
+            updatedTeam = team;
+        }
+    }
+    res.status(200).json({ updatedTeam });
 });
 
 module.exports = app;
