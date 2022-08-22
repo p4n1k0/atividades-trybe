78d6bed6b78a7926bad986b419d10fa81366ab00
Tarick <gabre.tarick@gmail.com>
2022-08-22 19:59:51 -0300

    cria servidor nodejs utilizando Express

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/exercises/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/exercises/app.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.get('/myActivities/:id', (req, res) => {
+    const activity = activities.find(({ id }) => id === Number(req.params.id));   
+    res.status(200).json(activity); 
+});
+
 module.exports = app;
