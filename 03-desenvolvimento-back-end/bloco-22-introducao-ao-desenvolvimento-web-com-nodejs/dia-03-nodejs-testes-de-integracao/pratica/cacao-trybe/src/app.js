df41aebee2629130d1cd5165bb2f62a47b327ec4
Gabriel Tarick <61570165+p4n1k0@users.noreply.github.com>
2022-08-23 23:00:58 -0300

    Revert "[Exercises]"

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-03-nodejs-testes-de-integracao/pratica/cacao-trybe/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-03-nodejs-testes-de-integracao/pratica/cacao-trybe/src/app.js
@@ -8,11 +8,6 @@ app.get('/chocolates', async (req, res) => {
   res.status(200).json({ chocolates });
 });
 
-app.get('/chocolates/total', async (req, res) => {
-  const chocolates = await cacaoTrybe.getAllChocolates();
-  res.status(200).json({ totalChocolates: chocolates.length });
-});
-
 app.get('/chocolates/:id', async (req, res) => {
     const { id } = req.params;
     const chocolate = await cacaoTrybe.getChocolateById(Number(id));
