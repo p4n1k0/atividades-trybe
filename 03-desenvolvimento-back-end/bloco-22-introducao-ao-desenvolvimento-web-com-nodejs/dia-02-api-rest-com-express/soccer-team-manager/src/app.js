54a20a081f6668cecb84ff68b98b7fdfcf003396
Tarick <gabre.tarick@gmail.com>
2022-08-24 22:54:51 -0300

    usa middlewares da comunidade e router middleware!

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/soccer-team-manager/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/soccer-team-manager/src/app.js
@@ -1,64 +1,14 @@
 const express = require('express');
-const validateTeam = require('./middlewares/validateTeam');
-const apiCredentials = require('./middlewares/apiCredentials');
+require('express-async-errors');
+const morgan = require('morgan');
+
+const teamsRouter = require('./routes/teamsRouter');
 
 const app = express();
-app.use(apiCredentials); 
-app.use(express.static('./images'));
-
-let nextId = 3;
-
-const teams = [
-    { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
-    { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
-  ];
-
+app.use(morgan('dev'));
+app.use(express.static('/images'));
 app.use(express.json());
-
-app.get('/teams', (req, res) => res.status(200).json({ teams }));
-
-const existingId = (req, res, next) => {
-    const id = Number(req.params.id);
-
-    if (teams.some((t) => t.id === id)) {
-        next();
-    }
-    res.sendStatus(404);
-};
-
-app.get('/teams/:id', existingId, (req, res) => {
-    const id = Number(req.params.id);
-    const team = teams.find((t) => t.id === id);
-    res.json(team);
-});
-
-app.post('/teams', validateTeam, (req, res) => {
-    if (!req.teams.includes(req.body.sigla) && teams.every((t) => t.sigla !== req.body.sigla)) {
-        return res.sendStatus(401);
-    }
-    const team = { id: nextId, ...req.body };
-    teams.push(team);
-    nextId += 1;
-    res.status(201).json(team);
-});
-
-app.put('/teams/:id', existingId, validateTeam, (req, res) => {
-    const id = Number(req.params.id);
-    const team = teams.find((t) => t.id === id);
-    const index = teams.indexOf(team);
-    const updated = { id, ...req.body };
-    teams.splice(index, 1, updated);
-    res.status(201).json(updated);    
-});
-
-app.delete('/teams/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const team = teams.find((t) => t.id === id);
-    const index = teams.indexOf(team);
-    teams.splice(index, 1);  
-    res.sendStatus(200);
-  });
-
-  app.use((req, res) => res.sendStatus(404));
+// monta o router na rota /teams (1)
+app.use('/teams', teamsRouter);
 
 module.exports = app;
