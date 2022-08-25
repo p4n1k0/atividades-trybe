375291ba8a12509eea1dc5f4aa80ac85c8d3750f
Tarick <gabre.tarick@gmail.com>
2022-08-25 16:10:46 -0300

    cria endpoints de listagem de pessoas2

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodejs-express-e-mysql/trybecash-api/tests/integration/people.test.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodejs-express-e-mysql/trybecash-api/tests/integration/people.test.js
@@ -9,6 +9,23 @@ const { expect, use } = chai;
 
 use(chaiHttp);
 
+const peopleList = [
+  {
+    id: 1,
+    first_name: 'Luke',
+    last_name: 'Skywalker',
+    email: 'luke.skywalker@trybe.com',
+    phone: '851 678 4453',
+  },
+  {
+    id: 2,
+    first_name: 'Dart',
+    last_name: 'Vader',
+    email: 'dart.vader@trybe.com',
+    phone: '851 678 5665',
+  },
+];
+
 describe('Testando os endpoints de people', () => {
   it('Testando o cadastro de uma pessoa ', async () => {
     sinon.stub(connection, 'execute').resolves([{ insertId: 42 }]);
@@ -28,6 +45,26 @@ describe('Testando os endpoints de people', () => {
     expect(response.status).to.equal(201);
     expect(response.body).to.
       deep.equal({ message: 'Pessoa cadastrada com sucesso com o id 42' });
+  });  
+
+  it('Testando a listagem de todas as pessoas', async () => {
+    sinon.stub(connection, 'execute').resolves([peopleList]);
+    const response = await chai
+      .request(app)
+      .get('/people');
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal(peopleList)
+  })
+
+  it('Testando a listagem da pessoa com o id 1', async () => {
+    sinon.stub(connection, 'execute').resolves([[peopleList[0]]]);
+    const response = await chai  
+      .request(app)
+      .get('/people/1');
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal(peopleList[0]);
   });
 
   afterEach(sinon.restore);
