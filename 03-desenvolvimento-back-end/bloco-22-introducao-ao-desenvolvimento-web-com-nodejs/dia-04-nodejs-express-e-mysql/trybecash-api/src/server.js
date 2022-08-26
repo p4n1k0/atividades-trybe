e92add95e55422c1dbbd84ca02c40cc6598df681
Tarick <gabre.tarick@gmail.com>
2022-08-25 22:31:24 -0300

    variaveis de ambiente

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodejs-express-e-mysql/trybecash-api/src/server.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodejs-express-e-mysql/trybecash-api/src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const app = require('./app');
 const connection = require('./db/connection')
 
@@ -5,6 +6,7 @@ const port = 3666;
 
 app.listen(port, async () => {
   console.log(`API TrybeCash está sendo executada na porta ${port}`);
+  console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
 
   const [result] = await connection.execute('SELECT 1');
 
