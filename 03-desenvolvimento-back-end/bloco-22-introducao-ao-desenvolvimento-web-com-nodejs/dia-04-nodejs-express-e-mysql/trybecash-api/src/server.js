3f3b1fbd89f8d21e33a2b708267e26940579a28d
Tarick <gabre.tarick@gmail.com>
2022-08-25 22:13:14 -0300

    cria endpoints de atualizacao e exclusao de pessoas

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodejs-express-e-mysql/trybecash-api/src/server.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodejs-express-e-mysql/trybecash-api/src/server.js
@@ -1,7 +1,17 @@
 const app = require('./app');
+const connection = require('./db/connection')
 
-const port = 3003;
+const port = 3666;
 
 app.listen(port, async () => {
   console.log(`API TrybeCash está sendo executada na porta ${port}`);
+
+  const [result] = await connection.execute('SELECT 1');
+
+  if (result) {
+    console.log('MySQL connection OK');
+  }
+
 });
+
+
