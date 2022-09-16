0cc81976fc7edd73f004180264206576aa3e7532
Tarick <gabre.tarick@gmail.com>
2022-09-15 23:00:10 -0300

    interface de uma aplicacao com o banco de dados

---
+++ 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/pratica/app-with-sequelize/src/models/user.model.js
@@ -0,0 +1,19 @@
+const UserModel = (sequelize, DataTypes) => {
+
+    const User = sequelize.define('User', {
+
+        fullName: DataTypes.STRING,
+
+        email: DataTypes.STRING,
+
+        phoneNum: DataTypes.STRING,
+
+    });
+
+
+    return User;
+
+};
+
+
+module.exports = UserModel;
