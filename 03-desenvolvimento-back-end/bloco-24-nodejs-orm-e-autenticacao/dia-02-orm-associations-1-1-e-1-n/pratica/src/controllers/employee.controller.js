063664ed0d085531adff478aa10fac368987f082
Tarick <gabre.tarick@gmail.com>
2022-09-17 22:51:39 -0300

    Managed transactions

--- 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations-1-1-e-1-n/pratica/src/controllers/employee.controller.js
+++ 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations-1-1-e-1-n/pratica/src/controllers/employee.controller.js
@@ -1,5 +1,8 @@
 const EmployeeService = require('../services/employee.service');
 const AddressService = require('../services/address.service');
+const EmployeeService = require('../services/employee.service');
+const AddressService = require('../services/address.service');
+
 
 
 const getAll = async (_req, res) => {
@@ -33,7 +36,22 @@ const getById = async (req, res) => {
   };
 }
 
+const insert = async (req, res) => {
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+    const employee = await EmployeeService.insert({ firstName, lastName, age, city, street, number });
+
+    return res.status(201).json({ id: employee.id, message: 'Cadastrado com sucesso' });
+  } catch (e) {
+    console.log(e);
+
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  };
+
+};
+
 module.exports = {
   getAll,
   getById,
+  insert,
 };
