03f0251dccc03e8f7fadb0066dab51e7e197ddc1
Tarick <gabre.tarick@gmail.com>
2022-09-19 20:34:48 -0300

    dropa db, soh p lembrar

--- 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-orm-associations-n-n-e-transactions/pratica/src/server.js
+++ 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-orm-associations-n-n-e-transactions/pratica/src/server.js
@@ -1,6 +1,4 @@
 const app = require('./app');
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log(`Escutando na porta ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Escutando na porta ${PORT}`));
