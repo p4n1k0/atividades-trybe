0ba80cb0016b2f5cd20a8622d31d68e6570e095d
Tarick <gabre.tarick@gmail.com>
2022-09-19 20:20:40 -0300

    altera scripts de test para o nodemon

--- 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-orm-associations-n-n-e-transactions/pratica/src/server.js
+++ 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-orm-associations-n-n-e-transactions/pratica/src/server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
-const { PORT = 3001 } = process.env;
+const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => `Ouvindo na porta `);
-
+app.listen(PORT, () => {
+    console.log(`Escutando na porta ${PORT}`);
+});
