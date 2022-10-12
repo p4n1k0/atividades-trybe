38264dec3a0ffc43475a63c1281a90bc091f6f84
Tarick <gabre.tarick@gmail.com>
2022-10-12 15:03:18 -0300

    script q converte unidades de medida de capacidade

--- 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/my-ts-scripts/capacity.ts
+++ 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/my-ts-scripts/capacity.ts
@@ -1,4 +1,4 @@
-const units = ["kg", "hg", "dag", "g", "dg", "cg", "mg"];
+const units = ["kl", "hl", "dal", "l", "dl", "cl", "ml"];
 
 function convert(value: number, fromUnit: string, toUnit: string): number {
     const fromIndex = units.indexOf(fromUnit);
