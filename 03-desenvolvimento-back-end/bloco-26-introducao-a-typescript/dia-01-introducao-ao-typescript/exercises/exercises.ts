35a73a1afc9e029a8a9adbbb7e9d1e5066be6972
Tarick <gabre.tarick@gmail.com>
2022-10-11 22:41:22 -0300

    funcao q add tds os nums dentro da array

--- 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/exercises/exercises.ts
+++ 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/exercises/exercises.ts
@@ -2,7 +2,14 @@ export function greeter(name: string): string {
     return `Olá ${name}!`;
 }
 
-
 export function personAge(name: string, age: number): string {
     return `${name} tem ${age} anos!`;
+}
+
+export function add(x: number, y: number): number {
+    return x + y;
+}
+
+export function sumArray(numbers: number[]): number {
+    return numbers.reduce(add, 0);
 }
