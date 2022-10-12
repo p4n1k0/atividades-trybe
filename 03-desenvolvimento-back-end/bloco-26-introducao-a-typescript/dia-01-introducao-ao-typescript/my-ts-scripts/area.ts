22dfeae149caf454b4b844d023d12c6686479111
Tarick <gabre.tarick@gmail.com>
2022-10-12 15:04:59 -0300

    script q converte unidades de medida de area

--- 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/my-ts-scripts/area.ts
+++ 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/my-ts-scripts/area.ts
@@ -1,9 +1,9 @@
-const units = ["kl", "hl", "dal", "l", "dl", "cl", "ml"];
+const units = ["km²", "hm²", "dam²", "m²", "dm²", "cm²", "mm²"];
 
 function convert(value: number, fromUnit: string, toUnit: string): number {
     const fromIndex = units.indexOf(fromUnit);
     const toIndex = units.indexOf(toUnit);
     const exponent = (toIndex - fromIndex);
 
-    return value * Math.pow(10, exponent);
+    return value * Math.pow(100, exponent);
 }
