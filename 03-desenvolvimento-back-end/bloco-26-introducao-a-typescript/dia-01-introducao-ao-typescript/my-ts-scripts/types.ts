8cac303cb2c22fce03c5a0aebbd073124d86f7a2
Tarick <gabre.tarick@gmail.com>
2022-10-13 12:17:20 -0300

    type union representando vogais do alfabeto

--- 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/my-ts-scripts/types.ts
+++ 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/my-ts-scripts/types.ts
@@ -19,3 +19,5 @@ type StatesOfMatter = "liquid" | "solid" | "gaseous";
 type IdentifyingDocument = string | number;
 
 type SO = "linux" | "mac os" | "windows";
+
+type Vowels = "a" | "e" | "i" | "o" | "u";
