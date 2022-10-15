7be7f8ddc927dccf155704fdefc7e7416a7b458e
Tarick <gabre.tarick@gmail.com>
2022-10-15 17:14:08 -0300

    funcao q executa qrys com recurso generics

---
+++ 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-02-tipagem-estatica-e-generics/src/main.ts
@@ -0,0 +1,10 @@
+import connection  from './models/connection';
+
+const main = async () => {
+  const result = await connection.execute('SELECT * FROM books');
+  const [rows] = result;
+
+  console.log(rows);
+}
+
+main();
