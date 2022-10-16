6f60b0d956e466a44e3afaa1072984dd9b58d182
Tarick <gabre.tarick@gmail.com>
2022-10-16 20:45:23 -0300

    add interface q representa entidade Book

--- 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-02-tipagem-estatica-e-generics/model-mysql/src/models/Book.ts
+++ 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-02-tipagem-estatica-e-generics/model-mysql/src/models/Book.ts
@@ -1,16 +1,24 @@
 import connection from "./connection";
-
 import { Pool } from "mysql2/promise";
+
+export interface Book {
+  id?: number,
+  title: string,
+  price: number,
+  author: string,
+  isbn: string,
+}
+
 export default class BookModel {
   private connection: Pool;
   constructor() {
     this.connection = connection;
   }
 
-  public async getAll() {
+  public async getAll(): Promise<Book[]> {
     const result = await this.connection.execute('SELECT * FROM books');
     const [rows] = result;
 
-    return rows;
+    return rows as Book[];
   }
 }
