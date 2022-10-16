e4c45220d5ebfe1ddeb64bf8f537d21e4f0d51ad
Tarick <gabre.tarick@gmail.com>
2022-10-15 23:06:52 -0300

    scpt q converte unidade de medida de massa

--- 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/my-ts-scripts/dist/length.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const units = ["km", "hm", "dam", "m", "dm", "cm", "mm"];
-function convert(value, fromUnit, toUnit) {
-    const fromIndex = units.indexOf(fromUnit); // pega index da unidade base no array
-    const toIndex = units.indexOf(toUnit); // pega index da unidade para a conversão
-    const exponent = (toIndex - fromIndex); // calcula o expoente a partir da diferença dos index
-    return value * Math.pow(10, exponent);
-}
---
+++ 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/my-ts-scripts/mass.ts
@@ -0,0 +1,9 @@
+const unit = ["kg", "hg", "dag", "g", "dg", "cg", "mg"];
+
+function convertMass(value: number, fromUnit: string, toUnit: string): number {
+    const fromIndex = units.indexOf(fromUnit);
+    const toIndex = units.indexOf(toUnit);
+    const exponent = (toIndex - fromIndex);
+
+    return value * Math.pow(10, exponent);
+}
