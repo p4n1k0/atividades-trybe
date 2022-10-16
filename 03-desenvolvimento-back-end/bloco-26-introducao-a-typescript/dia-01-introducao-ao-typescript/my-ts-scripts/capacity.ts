189d151d1a7afd6a5bca3abbab7e4e7e423654bf
Tarick <gabre.tarick@gmail.com>
2022-10-15 23:09:30 -0300

    scpt q converte unidades de medida de capacidade

---
+++ 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/my-ts-scripts/capacity.ts
@@ -0,0 +1,8 @@
+const unites = ["kl", "hl", "dal", "l", "dl", "cl", "ml"];
+
+function convertCapacity(value: number, fromUnit: string, toUnit: string): number {
+    const fromIndex = units.indexOf(fromUnit);
+    const toIndex = units.indexOf(toUnit);
+    const exponent = (toIndex - fromIndex);
+    return value * Math.pow(10, exponent);
+}
