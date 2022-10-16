5d8bbe8b4a6fb6e5c1632478bd80e27e1f0c0df9
Tarick <gabre.tarick@gmail.com>
2022-10-16 11:31:27 -0300

    remove arqvs e inicia init

--- 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-02-tipagem-estatica-e-generics/model-mysql/main.ts
+++
@@ -1,9 +0,0 @@
-import connection from './models/connection';
-
-const main = async () => {
-    const result = await connection.execute('SELECT * FROM books');
-    const [rows] = result;
-    console.log(rows);
-}
-
-main();
