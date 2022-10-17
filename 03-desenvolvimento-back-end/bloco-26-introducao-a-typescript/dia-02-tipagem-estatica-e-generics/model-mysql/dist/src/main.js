5f92fa33dd85e62590bb5c4fef09e15412284ec8
Tarick <gabre.tarick@gmail.com>
2022-10-16 21:11:12 -0300

    func p ler valores e cadastrar um livro atraves do metodo create

--- 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-02-tipagem-estatica-e-generics/model-mysql/dist/src/main.js
+++ 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-02-tipagem-estatica-e-generics/model-mysql/dist/src/main.js
@@ -12,10 +12,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const readline_sync_1 = __importDefault(require("readline-sync"));
 const Book_1 = __importDefault(require("./models/Book"));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const bookModel = new Book_1.default();
-    const books = yield bookModel.getAll();
-    console.log(books);
+    const title = readline_sync_1.default.question('Digite o título do livro: ');
+    const price = readline_sync_1.default.questionFloat('Digite o preço do livro: ');
+    const author = readline_sync_1.default.question('Digite o autor do livro: ');
+    const isbn = readline_sync_1.default.question('Digite o isbn do livro: ');
+    const newBook = { title, price, author, isbn };
+    const createdBook = yield bookModel.create(newBook);
+    console.log(createdBook);
 });
 main();
