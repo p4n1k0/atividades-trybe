a98609e763e81190537c441c8a638d34fdd67c77
Tarick <gabre.tarick@gmail.com>
2022-10-17 23:25:11 -0300

    add middleware de erro

--- 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-03-express-com-typescript/express-typescript/index.ts
+++ 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-03-express-com-typescript/express-typescript/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import statusCodes from './statusCodes';
 
 const app = express();
@@ -9,6 +9,36 @@ const PORT = 8000;
 
 app.get('/', (req: Request, res: Response) => {
     res.status(statusCodes.OK).send('Express + TypeScript')
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    const { name, message, details } = err as any;
+
+    console.log(`name: ${name}`);
+
+    switch (name) {
+        case 'ValidationError':
+            res.status(400).json({ message: details[0].message });
+            break;
+
+        case 'NotFoundError':
+            res.status(404).json({ message });
+            break;
+
+        case 'ConflictError':
+            res.status(409).json({ message });
+            break;
+
+        default:
+            console.error(err);
+
+            res.sendStatus(500);
+
+    }
+
+
+    next();
+
 });
 
 app.listen(PORT, () => {
