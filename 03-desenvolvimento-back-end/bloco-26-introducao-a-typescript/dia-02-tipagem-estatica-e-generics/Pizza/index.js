709303df4a39e039693fa03907834ea8d940133e
Tarick <gabre.tarick@gmail.com>
2022-10-17 22:44:21 -0300

    compila e executa

---
+++ 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-02-tipagem-estatica-e-generics/Pizza/index.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var calabresa = {
+    flavor: "Calabresa",
+    slices: 8
+};
+console.log(calabresa);
+var marguerita = {
+    flavor: "Marguerita",
+    slices: 6
+};
+console.log(marguerita);
+var nutela = {
+    flavor: "Nutela",
+    slices: 4
+};
+console.log(nutela);
