4c9063782299edfedd4f3b8c961544e99f03e7bb
Tarick <gabre.tarick@gmail.com>
2022-10-19 17:27:14 -0300

    func update na model

--- 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-03-express-com-typescript/express-typescript/models/book.model.ts
+++ 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-03-express-com-typescript/express-typescript/models/book.model.ts
@@ -42,4 +42,14 @@ export default class BookModel {
 
         return { id: insertId, ...book };
     }
+
+
+    public async update(id: number, book: Book) {
+        const { title, price, author, isbn } = book;
+ 
+        await this.connection.execute( 
+          'UPDATE books SET title=?, price=?, author=?, isbn=? WHERE id=?', 
+          [title, price, author, isbn, id] 
+        ); 
+      }
 }
