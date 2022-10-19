513ffc3fb914509e2eebe2d76f8fd80707e0869e
Tarick <gabre.tarick@gmail.com>
2022-10-19 17:03:00 -0300

    service

--- 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-03-express-com-typescript/express-typescript/services/books.service.ts
+++ 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-03-express-com-typescript/express-typescript/services/books.service.ts
@@ -9,10 +9,19 @@ class BookService {
         this.model = new BookModel(connection);
     }
 
+
     public async getAll(): Promise<Book[]> {
         const books = await this.model.getAll();
+
         return books;
     }
+
+
+    public async getById(id: number): Promise<Book> {
+        const book = await this.model.getById(id);
+    
+        return book;    
+      }
 }
 
 export default BookService;
