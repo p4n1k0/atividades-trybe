6c3c3ae276a0ade5ac0a5a0458e78c2697a4500b
p4n1k0 <gabre.tarick@gmail.com>
2023-03-08 19:30:07 -0300

    extrai dados cada vez q encontra uma nova pagina

--- 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/crawler/crawler.py
+++ 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/crawler/crawler.py
@@ -2,14 +2,17 @@ from parsel import Selector
 import requests
 
 
-response = requests.get('http://books.toscrape.com')
-selector = Selector(text=response.text)
-
-
-titles = selector.css('.product_pod h3 a::attr(title)').getall()
-prices = selector.css('.product_price .price_color::text').getall()
-
-for product in selector.css('.product_pod'):
-    title = product.css('h3 a::attr(title)').get()
-    price = product.css('.price_color::text').get()
-    print(title, price)
+# Define a primeira página como próxima a ter seu conteúdo recuperado
+URL_BASE = 'http://books.toscrape.com/catalogue/'
+next_page_url = 'page-1.html'
+while next_page_url:
+    # Busca o conteúdo da próxima página
+    response = requests.get(URL_BASE + next_page_url)
+    selector = Selector(text=response.text)
+    # Imprime os produtos de uma determinada página
+    for product in selector.css('.product_pod'):
+        title = product.css('h3 a::attr(title)').get()
+        price = product.css('.price_color::text').get()
+        print(title, price)
+    # Descobre qual é a próxima página
+    next_page_url = selector.css('.next a::attr(href)').get()
