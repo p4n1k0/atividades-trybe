aaffdd66a7bb9873b680178cf8fca72ed841f5c1
p4n1k0 <gabre.tarick@gmail.com>
2023-03-09 11:46:18 -0300

    faz limpeza de dados antes de estruturar e armazena-los

--- 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/crawler/teste.py
+++ 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/crawler/teste.py
@@ -2,26 +2,13 @@ from parsel import Selector
 import requests
 
 
-URL_BASE = 'http://books.toscrape.com/catalogue/'
-# Define a primeira página com próxima a ter seu conteúdo recuperado
-next_page_url = 'page-1.html'
-while next_page_url:
-    # Busca o conteúdo da próxiuma página
-    response = requests.get(URL_BASE + 'page-1.html')
-    selector = Selector(text=response.text)
-    # Imprime os produtos de uma determinada página
-    for product in selector.css('.product_pod'):
-        # Busca e extrai o título e o preço
-        title = product.css('h3 a::attr(title)').get()
-        price = product.css('.price_color::text').get()
-        print(title, price)
-        # Busca o detalhe de um produto
-        detail_href = product.css('h3 a::attr(href)').get()
-        detail_page_url = URL_BASE + detail_href
-        # Baixa o conteúdo da página de detalhes
-        detail_response = requests.get(detail_page_url)
-        detail_selector = Selector(text=detail_response.text)
-        # recupera a descrição do produto
-        # ~ significa a tag irmã
-        description = detail_selector.css('#product_description ~ p::text').get()
-        print(description)
+response = requests.get('http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html')
+selector = Selector(text=response.text)
+# Extrai a descrição
+description = selector.css('#product_description ~ p::text').get()
+print(description)
+# "Fatiamos" a descrição removendo o sufixo
+suffix = "...more"
+if description.endswith(suffix):
+    description = description.removesuffix(suffix)
+print(description)
