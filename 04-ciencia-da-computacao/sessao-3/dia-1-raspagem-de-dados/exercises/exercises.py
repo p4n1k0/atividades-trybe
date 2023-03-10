0fbcd4da9221b13fc5b6a3d0b7b290b9d27ce93f
p4n1k0 <gabre.tarick@gmail.com>
2023-03-09 22:48:59 -0300

    requisicao ao recurso de usuarios do github, exibe username e url

--- 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/exercises/exercises.py
+++ 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/exercises/exercises.py
@@ -1,5 +1,7 @@
 import requests
 
 
-response = requests.get('https://httpbin.org/encoding/utf8')
-print(response.text)
+response = requests.get('https://api.github.com/users')
+users = response.json()
+for user in users:
+    print(f"{user['login']} {user['url']}")
