d084947bf4ba55663e41487169a6fd936ce3239a
p4n1k0 <gabre.tarick@gmail.com>
2023-03-10 13:29:40 -0300

    extracao d campos, descricao e url com img da capa do livro

--- 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/exercises/exercises.py
+++ 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/exercises/exercises.py
@@ -1,7 +1,21 @@
 import requests
+import parsel
 
 
-response = requests.get('https://api.github.com/users')
-users = response.json()
-for user in users:
-    print(f"{user['login']} {user['url']}")
+URL_BASE = 'http://books.toscrape.com/catalogue/'
+response = requests.get(URL_BASE + 'the-grand-design_405/index.html')
+selector = parsel.Selector(response.text)
+
+title = selector.css('h1::text').get()
+
+price = selector.css('.product_main > price_color::text').re_first(r'\d*\.\d{2}')
+
+description = selector.css('#product_description ~p::text').get()
+suffix = '...more'
+if description.endswith(suffix):
+    description = description.removesuffix(suffix)
+
+cover = URL_BASE + selector.css('#product_gallery img::attr(src)').get()
+
+print(title, price, description, cover, sep=',')
+
