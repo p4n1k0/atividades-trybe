6f120f07f882681d20a75f74700630996d191a99
p4n1k0 <gabre.tarick@gmail.com>
2023-03-11 16:43:37 -0300

    paginacao e extracao de noticias do site

--- 04-ciencia-da-computacao/sessao-3/dia-2-outras-ferramentas-de-raspagem-de-dados/beautiful_soup/example.py
+++ 04-ciencia-da-computacao/sessao-3/dia-2-outras-ferramentas-de-raspagem-de-dados/beautiful_soup/example.py
@@ -55,5 +55,37 @@ def get_page_news(soup):
 
     return news
 
+# Recebe um objeto soup e retorna o link que redireciona
+# para a página seguinte, caso ele exista
+def get_next_page(soup_page):
+    try:
+        # Busca pela tag 'a' com as classes específicas do link desejado
+        return soup_page.find(
+            "a",
+            {"class": "tec--btn"},
+        )["href"]
+    except TypeError:
+        return None
+    
+# Recebe uma URL e retorna uma lista com todas as notícias do site
+def scrape_all(url):
+    all_news = []
+    # Enquanto a pesquisa pelo link que vai para a página seguinte existir
+    while get_next_page(get_html_soup(url)) is not None:
+        # Imprime a URL da página seguinte
 
-print(get_page_news(get_html_soup("https://www.tecmundo.com.br/novidades")))
+        print(get_next_page(get_html_soup(url)))
+
+        # Adiciona os elementos da lista com as notícias de cada
+        # página na lista 'all_news'
+        all_news.extend(get_page_news(get_html_soup(url)))
+        
+        # define a página seguinte como URL para a próxima iteração
+        url = get_next_page(get_html_soup(url))
+
+    return all_news
+
+# Vamos começar perto das últimas páginas pra não ter que fazer a requisição
+# do site inteiro
+print(scrape_all("https://www.tecmundo.com.br/novidades?page=11030"))
+
