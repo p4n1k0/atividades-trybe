3cd9ac375b62276400cd34ae82dbdf2e2f9acb7c
p4n1k0 <gabre.tarick@gmail.com>
2023-03-11 23:04:52 -0300

    extracao dos campos titulo, link/post, trecho exibido d cada post da pag

--- 04-ciencia-da-computacao/sessao-3/dia-2-outras-ferramentas-de-raspagem-de-dados/exercises/selenium_exercise.py
+++ 04-ciencia-da-computacao/sessao-3/dia-2-outras-ferramentas-de-raspagem-de-dados/exercises/selenium_exercise.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
 
-
-options = Options()
-options.add_argument('--headless') 
-
-firefox = webdriver.Firefox(options=options)
+firefox = webdriver.Firefox()
 
 def scrape(url):
     firefox.get(url)
-    paragraphs  = firefox.find_elements(By.TAG_NAME, 'p')
-    for p in paragraphs:
-        print(p.text)
 
-scrape('https://www.wikimetal.com.br/iron-maiden-scorpions-kiss-veja-melhores-albuns-1982/')
+    posts = []
+    for post in firefox.find_elements(By.CLASS_NAME, 'post-outer'):
+        new_item = dict()
+        new_item['title'] = post.find_element(By.CLASS_NAME, 'entry-title').find_element(By.TAG_NAME, 'a').get_attribute('innerHTML')
+        new_item['link'] = post.find_element(By.CLASS_NAME, 'entry-title').find_element(By.TAG_NAME, 'a').get_attribute('href')
+        new_item['excerpt'] = post.find_element(By.CLASS_NAME, 'entry-excerpt').get_attribute('innerHTML')
+        posts.append(new_item)
+    return posts
+
+print(scrape('https://diolinux.com.br/'))
