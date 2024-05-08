e5eaf732a52f8899b98aa52cb3bdf62de01cba06
p4n1k0 <gabre.tarick@gmail.com>
2024-05-08 19:19:28 -0300

    percorrendo e imprimindo tds os valores

--- 01-fundamentos/bloco-03-introcao-a-javascript/Dia_2-javascript-array-e-estrutura-de-recepcao/bonus.js
+++ 01-fundamentos/bloco-03-introcao-a-javascript/Dia_2-javascript-array-e-estrutura-de-recepcao/bonus.js
@@ -1,13 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let newNumbers = [];
-
-for(let index = 0; index < numbers.length; index += 1){
-    if(index + 1 < numbers.length){                          // Se index + 1 for menor que a array number 
-        newNumbers.push(numbers[index] * numbers[index + 1]); // Array newNumbers recebe indice 0 da array numbers multiplicado por ela mesma e soma + 1 para o próximo índice.
-    }else{
-        newNumbers.push(numbers[index] * 2); // Senão array newNumbers recebe índices da array numbers multiplicado por 2;
-    }
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index]);
 }
-
-console.log(newNumbers);
