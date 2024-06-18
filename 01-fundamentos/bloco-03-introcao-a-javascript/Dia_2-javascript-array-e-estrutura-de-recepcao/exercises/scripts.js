2b4e5a724e7b488a963d1c878f5f1b5b9374d660
p4n1k0 <gabre.tarick@gmail.com>
2024-06-18 17:02:21 -0300

    media > ou < 20

--- 01-fundamentos/bloco-03-introcao-a-javascript/Dia_2-javascript-array-e-estrutura-de-recepcao/exercises/scripts.js
+++ 01-fundamentos/bloco-03-introcao-a-javascript/Dia_2-javascript-array-e-estrutura-de-recepcao/exercises/scripts.js
@@ -1,10 +1,14 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
+let result = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
-  sum += numbers[index];
+  result += numbers[index];
 };
 
-let average = sum / numbers.length;
+result = result / numbers.length;
 
-console.log(average);
+if (result > 20) {
+  console.log('O valor da média aritmética é maior que 20');
+} else {
+  console.log('O valor da média aritmética é menor que 20');
+};
