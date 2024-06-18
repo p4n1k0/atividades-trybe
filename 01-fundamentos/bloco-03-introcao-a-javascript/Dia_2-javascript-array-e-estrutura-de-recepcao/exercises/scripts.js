2218d43445d2196e494c7d8cf8830ded488a5eca
p4n1k0 <gabre.tarick@gmail.com>
2024-06-18 17:05:59 -0300

    maior valor dentro do array

--- 01-fundamentos/bloco-03-introcao-a-javascript/Dia_2-javascript-array-e-estrutura-de-recepcao/exercises/scripts.js
+++ 01-fundamentos/bloco-03-introcao-a-javascript/Dia_2-javascript-array-e-estrutura-de-recepcao/exercises/scripts.js
@@ -1,14 +1,10 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let result = 0;
+let higherNumber = numbers[0];
 
 for (let index = 0; index < numbers.length; index += 1) {
-  result += numbers[index];
+  if (numbers[index] > higherNumber) {
+    higherNumber = numbers[index];
+  };
 };
 
-result = result / numbers.length;
-
-if (result > 20) {
-  console.log('O valor da média aritmética é maior que 20');
-} else {
-  console.log('O valor da média aritmética é menor que 20');
-};
+console.log(higherNumber);
