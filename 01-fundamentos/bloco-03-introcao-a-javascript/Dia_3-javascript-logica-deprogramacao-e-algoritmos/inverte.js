83f3ad49e71e0971cd172ef2a47caac833ce8913
p4n1k0 <gabre.tarick@gmail.com>
2024-06-18 17:17:03 -0300

    refatoracao e inversao de palavra

--- 01-fundamentos/bloco-03-introcao-a-javascript/Dia_3-javascript-logica-deprogramacao-e-algoritmos/inverte.js
+++
@@ -1,9 +0,0 @@
-let word = 'tryber';
-
-let novaPalavra = "";    //vai receber a nova palavra.
-
-for(let index = 0; index < word.length; index += 1){ // percorre cada letra de nossa primeira variável
-    novaPalavra += word[word.length - 1 - index]; // variavel vazia recebe a palavra trybe inverso.
-}
-
-console.log(novaPalavra);  //imprime a nova palavra 
