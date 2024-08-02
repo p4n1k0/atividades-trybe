4fd382be524776a999d9ecb67530bf2c9fcfa896
p4n1k0 <gabre.tarick@gmail.com>
2024-08-02 19:54:04 -0300

    testando se valor inserido é entre 1 e 10

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_1-javascript-es6-let-const-arrow-functions-e-templates-literals/aprendendo-um-pouco-mais/script.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia_1-javascript-es6-let-const-arrow-functions-e-templates-literals/aprendendo-um-pouco-mais/script.js
@@ -25,14 +25,49 @@ button.addEventListener('click', () => {
 const showPromo = (name, number) => {
     const firstText = document.querySelector('#text-initial');
     const secondText = document.querySelector('#text-final');
-    const productObject = checkNumber(parseInt(number));
 
-    firstText.innerHTML = `Boas-vindas, ${name}!`;
-    secondText.innerHTML = `A promoção do dia é: 
-    ${productObject.product} no valor de R$ ${productObject.price}`;
+    try {
+        checkName(name);
+        checkNumber(parseInt(number));
+        checkValidRage(number);
+        const productObject = checkPromo(number);
+
+        firstText.innerHTML = `Boas-vindas, ${name}!`;
+        secondText.innerHTML = `A promoção do dia é: 
+        ${productObject.product} no valor de R$ ${productObject.price}`;
+    } catch (err) {
+        secondText.innerHTML = err.message;
+    }
+}
+
+const checkName = (name) => {
+    let letters = /[aA-zZ]+/;
+
+    if (!name.match(letters)) {
+        throw new Error('É necessário digitar um nome válido');
+    }
 }
 
 // Segunda parte
 const checkNumber = (number) => {
-    return promo[number - 1];
+    if (isNaN(number)) {
+        throw new Error('É necessário digitar um número');
+    }
+}
+
+// Terceira parte
+const checkPromo = (number) => {
+    for (let index = 0; index < promo.length; index += 1) {
+        if (number - 1 === index) {
+            return promo[index];
+        }
+    }
+}
+
+// Quarta parte
+const checkValidRage = (number) => {
+    if (number < 1 || number > 10) {
+        throw new Error('É necessário digitar um número entre 1 e 10');
+        
+    }
 }
