e5d0028620d8da238b410e9c85b2855e83fc1e49
p4n1k0 <gabre.tarick@gmail.com>
2024-08-08 16:48:39 -0300

    pokemon decrescente por nivel

--- 01-fundamentos/bloco-7-metodos-de-array/Dia_2-metodos-de-array-filter-e-sort/exercises/bonus.js
+++ 01-fundamentos/bloco-7-metodos-de-array/Dia_2-metodos-de-array-filter-e-sort/exercises/bonus.js
@@ -35,6 +35,18 @@ const getStrongPokemonNames = (pokemons) => pokemons
 console.log(getStrongPokemonNames(pokemons));
 
 
-const sortPokemons = pokemons.sort((a, b) => a.level - b.level);
+const sortPokemons = (pokemons) => {
+    return pokemons.sort(({ level: aLevel, name: aName }, { level: bLevel, name: bName }) => {
+        if (aLevel === bLevel) {
+            return aName.localeCompare(bName)
+        } else {
+            return bLevel - aLevel;
+        }
+    });
+};
 
-console.log(sortPokemons);
+console.log(sortPokemons(pokemons));
+
+
+
+
