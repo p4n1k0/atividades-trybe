037b5241f2f168fdae0308aa042d17ae326ab722
p4n1k0 <gabre.tarick@gmail.com>
2024-08-12 18:44:11 -0300

    busca de ctt telefonico em uma lista

--- 01-fundamentos/bloco-8-metodos-de-array/metodos-de-array-find-e-foreach/array-find/find.js
+++ 01-fundamentos/bloco-8-metodos-de-array/metodos-de-array-find-e-foreach/array-find/find.js
@@ -7,3 +7,28 @@ const songs = [
 const foundSong = songs.find((song) => song.id === '31031685');
 
 console.log(foundSong);
+
+const contacts = [
+    { name: 'João', phone: '123456789' },
+    { name: 'Maria', phone: '987654321' },
+    { name: 'Pedro', phone: '456123789' },
+    { name: 'Ana', phone: '789456123' },
+    { name: 'Carlos', phone: '321654987' },
+    { name: 'Mariana', phone: '654123987' },
+    { name: 'Lucas', phone: '987321654' },
+    { name: 'Laura', phone: '123789456' },
+    { name: 'Tiago', phone: '789123456' },
+    { name: 'Beatriz', phone: '456789123' }
+];
+
+const searchContact = (contacts, name) => {
+    const foundCount = contacts.find(contact => contact.name === name);
+
+    if (foundCount) {
+        return { ...foundCount };
+    }
+    return 'Contato não encontrado'
+};
+
+console.log(searchContact(contacts, 'Ana'));
+
