fe2e3e478930f9c94d8404771969c0ea177b30ab
p4n1k0 <gabre.tarick@gmail.com>
2024-08-13 11:54:20 -0300

    relatorio mostrando materia em q estudante foi melhor

--- 01-fundamentos/bloco-8-metodos-de-array/Dia_4-metodos-de-array-reduce/array-reduce/reduce.js
+++ 01-fundamentos/bloco-8-metodos-de-array/Dia_4-metodos-de-array-reduce/array-reduce/reduce.js
@@ -4,3 +4,90 @@ const sum = (acc, number) => ((number % 2 === 0) ? acc + number : acc);
 const sumNumbers = (array) => array.reduce(sum, 0);
 
 console.log(sumNumbers(numbers));
+
+
+const students = [
+    {
+        name: 'Jorge',
+        lastName: 'Silva',
+        age: 14,
+        turn: 'Manhã',
+        courses: [
+            { name: 'Matemática', grade: 67 },
+            { name: 'Português', grade: 79 },
+            { name: 'Química', grade: 70 },
+            { name: 'Biologia', grade: 65 },
+        ],
+    },
+    {
+        name: 'Mario',
+        lastName: 'Ferreira',
+        age: 15,
+        turn: 'Tarde',
+        courses: [
+            { name: 'Matemática', grade: 59 },
+            { name: 'Português', grade: 80 },
+            { name: 'Química', grade: 78 },
+            { name: 'Biologia', grade: 92 },
+        ],
+    },
+    {
+        name: 'Jorge',
+        lastName: 'Santos',
+        age: 15,
+        turn: 'Manhã',
+        courses: [
+            { name: 'Matemática', grade: 76 },
+            { name: 'Português', grade: 90 },
+            { name: 'Química', grade: 70 },
+            { name: 'Biologia', grade: 80 },
+        ],
+    },
+    {
+        name: 'Maria',
+        lastName: 'Silveira',
+        age: 14,
+        turn: 'Manhã',
+        courses: [
+            { name: 'Matemática', grade: 91 },
+            { name: 'Português', grade: 85 },
+            { name: 'Química', grade: 92 },
+            { name: 'Biologia', grade: 90 },
+        ],
+    },
+    {
+        name: 'Natalia',
+        lastName: 'Castro',
+        age: 14,
+        turn: 'Manhã',
+        courses: [
+            { name: 'Matemática', grade: 70 },
+            { name: 'Português', grade: 70 },
+            { name: 'Química', grade: 60 },
+            { name: 'Biologia', grade: 50 },
+        ],
+    },
+    {
+        name: 'Wilson',
+        lastName: 'Martins',
+        age: 14,
+        turn: 'Manhã',
+        courses: [
+            { name: 'Matemática', grade: 80 },
+            { name: 'Português', grade: 82 },
+            { name: 'Química', grade: 79 },
+            { name: 'Biologia', grade: 75 },
+        ],
+    },
+];
+
+const getBestClass = (acc, course) => (acc.grade > course.grade) ? acc : course;
+
+const reportBest = (students) => students.map((student) => {
+    return {
+        name: student.name,
+        course: student.courses.reduce(getBestClass).name,
+    };
+});
+
+console.log(reportBest(students));
