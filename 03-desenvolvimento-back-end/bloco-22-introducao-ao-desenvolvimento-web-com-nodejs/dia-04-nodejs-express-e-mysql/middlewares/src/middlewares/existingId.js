3da534d20f511008a879fff539a9fffcd4093c00
p4n1k0 <gabre.tarick@gmail.com>
2024-09-09 20:25:59 -0300

    retorno com obj

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodejs-express-e-mysql/middlewares/src/middlewares/existingId.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodejs-express-e-mysql/middlewares/src/middlewares/existingId.js
@@ -1,13 +1,9 @@
 const existingId = (req, res, next) => {
     const id = Number(req.params.id);
-
-    if (teams.some((t) => t.id === id)) {
-        // se existe, a requisição segue para o próximo middleware
-        next();
-    } else {
-        // se não existe, então vamos retornar o status HTTP 404
-        res.sendStatus(404);
+    if (!teams.some((t) => t.id === id)) {
+      return res.sendStatus(404).json({ message: 'Time não encontrado' });
     }
-};
+    next();
+  };
 
 module.exports = existingId;
