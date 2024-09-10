8c4003d85b85925aaed2f7cfbc6d5d541edd0b73
p4n1k0 <gabre.tarick@gmail.com>
2024-09-10 13:06:43 -0300

    middleware de validacao p chave name

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/tech-lead/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/tech-lead/src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const validateName = require('./middlewares/validateName');
 const app = express();
 
 app.use(express.json());
 
-app.post('/activities', (_req, res) => {
+app.post('/activities', validateName, (_req, res) => {
     res.status(201).json({ message: 'Atividade registrada com sucesso!' });
 });
 
