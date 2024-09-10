cdb70c391d3c9fc3a0b8303d03b9505cc4dcc30e
p4n1k0 <gabre.tarick@gmail.com>
2024-09-10 18:11:24 -0300

    middleware p validacao de chave description

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/tech-lead/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/tech-lead/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
+const validateDescription = require('./middlewares/validateDescription');
 const app = express();
 
 app.use(express.json());
 
 app.post('/activities',
+    validateDescription,
     validateName,
     validatePrice,
     (_req, res) => {
