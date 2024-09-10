51b3fc0577c48e51018d43d9c2b3d1ab3aebc2c7
p4n1k0 <gabre.tarick@gmail.com>
2024-09-10 18:17:01 -0300

    middleware de validacao p chave createAt

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/tech-lead/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/tech-lead/src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
 const validateDescription = require('./middlewares/validateDescription');
+const validateCreatedAt = require('./middlewares/validateCreatedAt');
 const app = express();
 
 app.use(express.json());
 
 app.post('/activities',
+    validateCreatedAt,
     validateDescription,
     validateName,
     validatePrice,
