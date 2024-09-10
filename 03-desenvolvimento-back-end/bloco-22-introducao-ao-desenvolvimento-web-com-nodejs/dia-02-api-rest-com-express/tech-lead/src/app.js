65e3bbc2a9c0b8a8a0b90561c853a901e3c4dcca
p4n1k0 <gabre.tarick@gmail.com>
2024-09-10 18:42:29 -0300

    endpoint post para cadastro  + gerar token

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/tech-lead/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-api-rest-com-express/tech-lead/src/app.js
@@ -3,11 +3,28 @@ const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
 const validateDescription = require('./middlewares/validateDescription');
 const validateCreatedAt = require('./middlewares/validateCreatedAt');
+const validateRating = require('./middlewares/validateRating');
+const validateDifficulty = require('./middlewares/validateDifficulty');
+const generateToken = require('./utils/generateToken');
 const app = express();
 
 app.use(express.json());
 
+app.post('/signup', (_req, res) => {
+    const { email, password, firstName, phone } = req.body;
+
+    if ([email, password, firstName, phone].includes(undefined)) {
+        return res.status(401).json({ message: 'Campos ausentes' });
+    }
+
+    const token = generateToken();
+
+    return res.status(200).json({ token });
+});
+
 app.post('/activities',
+    validateDifficulty,
+    validateRating,
     validateCreatedAt,
     validateDescription,
     validateName,
