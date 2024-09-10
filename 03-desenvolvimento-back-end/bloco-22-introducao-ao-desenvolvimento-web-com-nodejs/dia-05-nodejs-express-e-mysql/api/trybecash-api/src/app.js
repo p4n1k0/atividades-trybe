452823bb7fcf6fce77a23ffc5c0f06c7a094c88e
p4n1k0 <gabre.tarick@gmail.com>
2024-09-10 20:15:51 -0300

    conecta mysql c express

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-nodejs-express-e-mysql/api/trybecash-api/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-nodejs-express-e-mysql/api/trybecash-api/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
-const app = express()
+const app = express();
+
+app.use(express.json());
 
 module.exports = app;
--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-nodejs-express-e-mysql/api/trybecash-api/src/server.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-nodejs-express-e-mysql/api/trybecash-api/src/server.js
@@ -1,3 +1,14 @@
 const app = require('./app');
+const connection = require('./db/connection');
 
-app.listen(3002, () => console.log('server running on port 3002'));
+const PORT = 3002;
+
+app.listen(PORT, async () => {
+    console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
+
+    // O código abaixo é para testarmos a comunicação com o MySQL
+    const [result] = await connection.execute('SELECT 1');
+    if (result) {
+        console.log('MySQL connection OK');
+    }
+});
