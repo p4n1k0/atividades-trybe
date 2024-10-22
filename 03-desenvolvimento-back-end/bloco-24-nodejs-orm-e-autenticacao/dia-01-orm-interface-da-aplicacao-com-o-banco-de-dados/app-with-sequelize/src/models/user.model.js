8a54643c849387bc50443dac0360fc403b5dccff
p4n1k0 <gabre.tarick@gmail.com>
2024-10-22 18:57:58 -0300

    migrations

--- 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/src/models/user.model.js
+++ 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/src/models/user.model.js
@@ -2,8 +2,11 @@ const UserModel = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
         fullName: DataTypes.STRING,
         email: DataTypes.STRING,
+        phoneNum: DataTypes.STRING,
     });
-
+    (async () => {
+        await sequelize.sync({ force: true });
+    })();
     return User;
 };
 
