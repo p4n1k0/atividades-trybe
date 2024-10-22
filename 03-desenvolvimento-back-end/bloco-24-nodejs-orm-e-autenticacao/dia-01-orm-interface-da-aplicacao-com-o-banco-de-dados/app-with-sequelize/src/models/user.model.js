9f30a4a3ba740cbbb2d52b6c3cb650f0bf97ab1c
p4n1k0 <gabre.tarick@gmail.com>
2024-10-22 18:41:52 -0300

    configura sequelize e inicia model

--- 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/src/models/user.model.js
+++ 03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/src/models/user.model.js
@@ -1,12 +1,8 @@
-UserModel = (sequelize, DataTypes) => {
+const UserModel = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
         fullName: DataTypes.STRING,
         email: DataTypes.STRING,
-        phoneNum: DataTypes.STRING,
     });
-    (async () => {
-        await sequelize.sync({ force: true });
-    })();
 
     return User;
 };
