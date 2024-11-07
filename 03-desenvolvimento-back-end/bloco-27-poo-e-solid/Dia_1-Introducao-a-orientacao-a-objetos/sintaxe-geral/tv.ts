1df8ed6c2c30b5d87f5427177b5a59bb068bec75
p4n1k0 <gabre.tarick@gmail.com>
2024-11-06 21:08:27 -0300

    classe TV private

--- 03-desenvolvimento-back-end/bloco-27-poo-e-solid/Dia_1-Introducao-a-orientacao-a-objetos/sintaxe-geral/tv.ts
+++ 03-desenvolvimento-back-end/bloco-27-poo-e-solid/Dia_1-Introducao-a-orientacao-a-objetos/sintaxe-geral/tv.ts
@@ -1,29 +1,24 @@
 class Tv {
-    brand: string;
-    size: number;
-    resolution: string;
-    connections: string[];
-    connectedTo?: string;
+    private _brand: string;
+    private _size: number;
+    private _resolution: string;
+    private _connections: string[];
+    private _connectedTo?: string;
 
-    constructor(
-        brand: string,
-        size: number,
-        resolution: string,
-        connections: string[],
-    ) {
-        this.brand = brand;
-        this.size = size;
-        this.resolution = resolution;
-        this.connections = connections;
+    constructor(brand: string, size: number, resolution: string, connections: string[]) {
+        this._brand = brand;
+        this._size = size;
+        this._resolution = resolution;
+        this._connections = connections;
     }
 
     turnOn(): void {
         console.log(
-            `TV ${this.brand}, ${this.size}",
-            resolution: ${this.resolution} \n\ available connections: ${this.connections}`
-        );    
-
-        const tv1 = new Tv('LG', 32, '4K', ['HDMI', 'Ethernet', 'Wifi']);
-        tv1.turnOn();
+            `TV ${this._brand}, ${this._size}", resolution: ${this._resolution} \n\  
+  available connections: ${this._connections}`,
+        );
     }
 }
+
+const tv1 = new Tv('LG', 32, '4K', ['HDMI', 'Ethernet', 'Wifi']);
+tv1.turnOn();
