30280d13614a31c6bd40f303ad3c5816b34c98a3
p4n1k0 <gabre.tarick@gmail.com>
2024-11-11 18:47:42 -0300

    implementando interfaces

--- 03-desenvolvimento-back-end/bloco-27-poo-e-solid/Dia_2-heranca-e-composicao/sintaxe-geral/Superclass.ts
+++ 03-desenvolvimento-back-end/bloco-27-poo-e-solid/Dia_2-heranca-e-composicao/sintaxe-geral/Superclass.ts
@@ -1,38 +1,31 @@
-// Crie uma classe chamada _Superclass_.
 class Superclass {
-    // A _Superclass_ deve possuir um atributo público _isSuper_.
     isSuper: boolean;
 
     constructor() {
-        // _isSuper_ deve ser setado como `true` na inicialização.
         this.isSuper = true;
     }
 
-    // A _Superclass_ deve possuir um método público chamado `sayHello`, que deve imprimir "Olá mundo!".
     public sayHello(): void {
         console.log('Olá mundo!');
     }
 }
 
-// Crie uma classe chamada _Subclass_ que herda da _Superclass_.
 class Subclass extends Superclass {
-    // Crie , na _Subclass_, um método público chamado `sayHello2`.
-    public sayHello2(): void {
-        // Chame o método `sayHello`, dentro do método `sayHello2`.
-        this.sayHello();
+    // No construtor da _Subclass_, o atributo _isSuper_ deve ser setado como `false`. Você vai precisar utilizar o _super_.
+    constructor() {
+        super();
+        this.isSuper = false;
     }
 }
 
-// Crie uma função `myFunc` fora do escopo da _Subclass_ que recebe um objeto da _Superclass_.
-const myFunc = (object: Subclass) => {
-    // Dentro dessa função, chame o método `sayHello` do objeto passado como parâmetro.
-    object.sayHello2();
+const myFunc = (object: Superclass) => {
+    object.sayHello();
+    // Dentro da função que recebe um objeto da _Superclass_ como parâmetro, cheque o valor do atributo _isSuper_ e imprima no console "Super!" se for `true` e "Sub!" se for `false`;
+    console.log(object.isSuper ? 'Super!' : 'Sub!');
 };
 
-// Crie um objeto da _Superclass_ e outro da _Subclass_.
 const sup = new Superclass();
 const sub = new Subclass();
 
-// Chame a função `myFunc` 2 vezes, passando os objetos criados.
-// myFunc(sup);
+myFunc(sup);
 myFunc(sub);
