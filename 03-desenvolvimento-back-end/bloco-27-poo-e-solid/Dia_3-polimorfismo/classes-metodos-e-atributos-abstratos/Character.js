652a05ed707cb685f319b6779be00c3c0667ac50
p4n1k0 <gabre.tarick@gmail.com>
2024-11-11 20:15:05 -0300

    compila

--- 03-desenvolvimento-back-end/bloco-27-poo-e-solid/Dia_3-polimorfismo/classes-metodos-e-atributos-abstratos/Character.js
+++ 03-desenvolvimento-back-end/bloco-27-poo-e-solid/Dia_3-polimorfismo/classes-metodos-e-atributos-abstratos/Character.js
@@ -1,68 +1,211 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
     };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Character = /** @class */ (function () {
-    function Character() {
+    return __assign.apply(this, arguments);
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-    Character.characterPresentation = function (character) {
-        character.talk();
-        character.specialMove();
+};
+var db = [];
+var LocalDbModel = /** @class */ (function () {
+    function LocalDbModel() {
+    }
+    LocalDbModel.prototype.create = function (character) {
+        return __awaiter(this, void 0, void 0, function () {
+            var lastId, newCharacter;
+            return __generator(this, function (_a) {
+                lastId = db.length > 0 ? db[db.length - 1].id : 0;
+                newCharacter = __assign({ id: lastId + 1 }, character);
+                db.push(newCharacter);
+                return [2 /*return*/, newCharacter];
+            });
+        });
     };
-    return Character;
+    ;
+    LocalDbModel.prototype.findIndexById = function (id) {
+        return __awaiter(this, void 0, void 0, function () {
+            var index;
+            return __generator(this, function (_a) {
+                index = db.findIndex(function (character) { return character.id === id; });
+                if (index < 0)
+                    throw new Error('Character not found');
+                return [2 /*return*/, index];
+            });
+        });
+    };
+    ;
+    LocalDbModel.prototype.update = function (id, character) {
+        return __awaiter(this, void 0, void 0, function () {
+            var indexToUpdate;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.findIndexById(id)];
+                    case 1:
+                        indexToUpdate = _a.sent();
+                        db[indexToUpdate] = __assign(__assign({}, db[indexToUpdate]), character);
+                        return [2 /*return*/, db[indexToUpdate]];
+                }
+            });
+        });
+    };
+    ;
+    LocalDbModel.prototype["delete"] = function (id) {
+        return __awaiter(this, void 0, void 0, function () {
+            var indexToDelete, deletedItem;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.findIndexById(id)];
+                    case 1:
+                        indexToDelete = _a.sent();
+                        deletedItem = db.splice(indexToDelete, 1);
+                        if (deletedItem.length > 0)
+                            return [2 /*return*/, true];
+                        return [2 /*return*/, false];
+                }
+            });
+        });
+    };
+    ;
+    LocalDbModel.prototype.getAll = function () {
+        return __awaiter(this, void 0, void 0, function () { return __generator(this, function (_a) {
+            return [2 /*return*/, db];
+        }); });
+    };
+    ;
+    LocalDbModel.prototype.getById = function (id) {
+        return __awaiter(this, void 0, void 0, function () {
+            var indexToGet;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.findIndexById(id)];
+                    case 1:
+                        indexToGet = _a.sent();
+                        return [2 /*return*/, db[indexToGet]];
+                }
+            });
+        });
+    };
+    ;
+    return LocalDbModel;
 }());
-var MeleeCharacter = /** @class */ (function (_super) {
-    __extends(MeleeCharacter, _super);
-    function MeleeCharacter(_name, _specialMoveName) {
-        var _this = _super.call(this) || this;
-        _this._name = _name;
-        _this._specialMoveName = _specialMoveName;
-        return _this;
+var CharacterService = /** @class */ (function () {
+    function CharacterService(model) {
+        this.model = model;
     }
-    MeleeCharacter.prototype.talk = function () {
-        /* Entra aqui uma implementação exclusiva para os personagens de curto alcance */
-        console.log("Hi, I'm " + this._name + ". I attack at close range.");
+    CharacterService.prototype.create = function (character) {
+        return __awaiter(this, void 0, void 0, function () {
+            var newCharacter;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.model.create(character)];
+                    case 1:
+                        newCharacter = _a.sent();
+                        return [2 /*return*/, ({ status: 201, data: newCharacter })];
+                }
+            });
+        });
     };
-    MeleeCharacter.prototype.specialMove = function () {
-        /* Entra aqui uma implementação exclusiva para os personagens de curto alcance */
-        console.log(this._name + " used " + this._specialMoveName + "!");
+    CharacterService.prototype.getAll = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var allCharacter;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.model.getAll()];
+                    case 1:
+                        allCharacter = _a.sent();
+                        return [2 /*return*/, ({ status: 200, data: allCharacter })];
+                }
+            });
+        });
     };
-    return MeleeCharacter;
-}(Character));
-var LongRangeCharacter = /** @class */ (function (_super) {
-    __extends(LongRangeCharacter, _super);
-    function LongRangeCharacter(_name, _specialMoveName) {
-        var _this = _super.call(this) || this;
-        _this._name = _name;
-        _this._specialMoveName = _specialMoveName;
-        return _this;
+    return CharacterService;
+}());
+var MockDbModel = /** @class */ (function () {
+    function MockDbModel() {
     }
-    LongRangeCharacter.prototype.talk = function () {
-        /* Entra aqui uma implementação exclusiva para os personagens de longo alcance */
-        console.log("Hi, I'm " + this._name + ". I can attack from a long range.");
+    MockDbModel.prototype.create = function (character) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                console.log('character created');
+                return [2 /*return*/, { id: 1, name: 'Peach', specialMove: 'Toad' }];
+            });
+        });
     };
-    LongRangeCharacter.prototype.specialMove = function () {
-        /* Entra aqui uma implementação exclusiva para os personagens de longo alcance */
-        console.log(this._name + " used " + this._specialMoveName + "!");
+    MockDbModel.prototype.update = function (id, character) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                console.log('character updated');
+                return [2 /*return*/, { id: 1, name: 'Yoshi', specialMove: 'Egg Lay' }];
+            });
+        });
     };
-    return LongRangeCharacter;
-}(Character));
-var yoshi = new MeleeCharacter('Yoshi', 'Super dragon');
-var samus = new LongRangeCharacter('Samus', 'Zero Laser');
-// yoshi.talk();
-// yoshi.specialMove();
-// samus.talk();
-// samus.specialMove();
-Character.characterPresentation(yoshi);
-Character.characterPresentation(samus);
+    MockDbModel.prototype["delete"] = function (id) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                console.log('character deleted');
+                return [2 /*return*/, true];
+            });
+        });
+    };
+    MockDbModel.prototype.getAll = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                return [2 /*return*/, [
+                        { id: 1, name: 'Samus', specialMove: 'Charge Shot' },
+                        { id: 2, name: 'Kirby', specialMove: 'Inhale' },
+                    ]];
+            });
+        });
+    };
+    MockDbModel.prototype.getById = function (id) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                return [2 /*return*/, { id: 1, name: 'Mario', specialMove: 'Fireball' }];
+            });
+        });
+    };
+    return MockDbModel;
+}());
+var A = new CharacterService(new LocalDbModel());
+A.getAll().then(console.log);
+var B = new CharacterService(new MockDbModel());
+B.getAll().then(console.log);
