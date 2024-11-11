552131a38c16807bf6b45a605f56a6a454906f05
p4n1k0 <gabre.tarick@gmail.com>
2024-11-11 20:11:47 -0300

    sintaxe de interfaces e generics

--- 03-desenvolvimento-back-end/bloco-27-poo-e-solid/Dia_3-polimorfismo/classes-metodos-e-atributos-abstratos/Character.ts
+++ 03-desenvolvimento-back-end/bloco-27-poo-e-solid/Dia_3-polimorfismo/classes-metodos-e-atributos-abstratos/Character.ts
@@ -1,52 +1,103 @@
-abstract class Character {
-    abstract talk(): void;
-    abstract specialMove(): void;
+interface Character {
+    name: string;
+    specialMove: string;
+}
 
-    static characterPresentation(character: Character): void {
-        character.talk();
-        character.specialMove();
+interface DbCharacter extends Character {
+    id: number;
+}
+
+const db: DbCharacter[] = [];
+
+interface IModel {
+    create(character: Character): Promise<DbCharacter>;
+    update(id: number, character: Character): Promise<DbCharacter>;
+    delete(id: number): Promise<boolean>;
+    getAll(): Promise<DbCharacter[]>;
+    getById(id: number): Promise<DbCharacter>;
+}
+
+class LocalDbModel implements IModel {
+    async create(character: Character) {
+        const lastId = db.length > 0 ? db[db.length - 1].id : 0;
+        const newCharacter = { id: lastId + 1, ...character };
+        db.push(newCharacter);
+        return newCharacter;
+    };
+
+    async findIndexById(id: number) {
+        const index = db.findIndex((character) => character.id === id);
+        if (index < 0) throw new Error('Character not found');
+        return index;
+    };
+
+    async update(id: number, character: Character) {
+        const indexToUpdate = await this.findIndexById(id);
+        db[indexToUpdate] = { ...db[indexToUpdate], ...character };
+        return db[indexToUpdate];
+    };
+
+    async delete(id: number) {
+        const indexToDelete = await this.findIndexById(id);
+        const deletedItem = db.splice(indexToDelete, 1);
+        if (deletedItem.length > 0) return true;
+        return false;
+    };
+
+    async getAll() { return db; };
+
+    async getById(id: number) {
+        const indexToGet = await this.findIndexById(id);
+        return db[indexToGet];
+    };
+}
+
+class CharacterService {
+    constructor(readonly model: IModel) { }
+
+    async create(character: Character) {
+        const newCharacter = await this.model.create(character);
+        return ({ status: 201, data: newCharacter });
+    }
+
+    async getAll() {
+        const allCharacter = await this.model.getAll();
+        return ({ status: 200, data: allCharacter });
+    }
+
+    /* Implementação dos outros métodos */
+}
+
+class MockDbModel implements IModel {
+    async create(character: Character) {
+        console.log('character created');
+        return { id: 1, name: 'Peach', specialMove: 'Toad' };
+    }
+
+    async update(id: number, character: Character) {
+        console.log('character updated');
+        return { id: 1, name: 'Yoshi', specialMove: 'Egg Lay' };
+    }
+
+    async delete(id: number) {
+        console.log('character deleted');
+        return true;
+    }
+
+    async getAll() {
+        return [
+            { id: 1, name: 'Samus', specialMove: 'Charge Shot' },
+            { id: 2, name: 'Kirby', specialMove: 'Inhale' },
+        ];
+    }
+
+    async getById(id: number) {
+        return { id: 1, name: 'Mario', specialMove: 'Fireball' };
     }
 }
 
+const A = new CharacterService(new LocalDbModel());
+A.getAll().then(console.log);
 
-class MeleeCharacter extends Character {
-    constructor(private _name: string, private _specialMoveName: string) {
-        super();
-    }
-
-    talk(): void {
-        /* Entra aqui uma implementação exclusiva para os personagens de curto alcance */
-        console.log(`Hi, I'm ${this._name}. I attack at close range.`);
-    }
-
-    specialMove(): void {
-        /* Entra aqui uma implementação exclusiva para os personagens de curto alcance */
-        console.log(`${this._name} used ${this._specialMoveName}!`);
-    }
-}
-
-class LongRangeCharacter extends Character {
-    constructor(private _name: string, private _specialMoveName: string) {
-        super();
-    }
-
-    talk(): void {
-        /* Entra aqui uma implementação exclusiva para os personagens de longo alcance */
-        console.log(`Hi, I'm ${this._name}. I can attack from a long range.`);
-    }
-
-    specialMove(): void {
-        /* Entra aqui uma implementação exclusiva para os personagens de longo alcance */
-        console.log(`${this._name} used ${this._specialMoveName}!`);
-    }
-}
-
-const yoshi = new MeleeCharacter('Yoshi', 'Super dragon');
-const samus = new LongRangeCharacter('Samus', 'Zero Laser');
-
-// yoshi.talk();
-// yoshi.specialMove();
-// samus.talk();
-// samus.specialMove();
-Character.characterPresentation(yoshi);
-Character.characterPresentation(samus);
+const B = new CharacterService(new MockDbModel());
+B.getAll().then(console.log);
