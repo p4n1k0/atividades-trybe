e2640295f7dabbc322b76b194a8080159d0c4d3c
p4n1k0 <gabre.tarick@gmail.com>
2024-12-04 15:04:40 -0300

    palavras lidas de um arqv

--- 04-ciencia-da-computacao/sessao-1/dia-2-entrada-e-saida-de-dados/exercises/scrambedWord.py
+++ 04-ciencia-da-computacao/sessao-1/dia-2-entrada-e-saida-de-dados/exercises/scrambedWord.py
@@ -1,24 +1,17 @@
 import random
 
-WORDS = [
-    "cat",
-    "elephant",
-    "dog",
-    "monkey",
-    "duck",
-    "chameleon",
-    "bear",
-    "moose",
-    "rooster",
-]
+
 MAX_ATTEMPTS = 3
+
+
+def retrieve_words(file):
+    return [word.strip() for word in file]
 
 
 def draw_secret_word(words):
     secret_word = random.choice(words)
     scrambled_word = "".join(random.sample(secret_word, len(secret_word)))
     return secret_word, scrambled_word
-
 
 def collect_guesses():
     guesses = []
@@ -27,16 +20,17 @@ def collect_guesses():
         guesses.append(guess)
     return guesses
 
-
 def check_game_result(secret_word, guesses):
     if secret_word in guesses:
-        print(f"You win: {secret_word}")
+        print(f"You win: the secret word is {secret_word}")
     else:
-        print(f"You lose: {secret_word}")
+        print(f"You lose: the secret word is {secret_word}")
 
 
 if __name__ == "__main__":
-    secret_word, scrambled_word = draw_secret_word(WORDS)
+    with open("words.txt") as file:
+        words = retrieve_words(file)
+    secret_word, scrambled_word = draw_secret_word(words)
     print(f"Scrambled word is {scrambled_word}")
     guesses = collect_guesses()
     check_game_result(secret_word, guesses)
