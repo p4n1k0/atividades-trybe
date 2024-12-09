cfe7e285f5134a92a81f82e2894570be7723f58e
p4n1k0 <gabre.tarick@gmail.com>
2024-12-08 22:00:44 -0300

    exercises

---
+++ 04-ciencia-da-computacao/sessao-1/dia-3-testes/testes-automatizados/emails.py
@@ -0,0 +1,12 @@
+from validate import validate_email
+
+def filter_valid_emails(emails):
+    valid_emails = []
+    for email in emails:
+        try:
+            validate_email(email)
+        except ValueError as exc:
+            print(exc)
+        else:
+            valid_emails.append(email)
+    return valid_emails
