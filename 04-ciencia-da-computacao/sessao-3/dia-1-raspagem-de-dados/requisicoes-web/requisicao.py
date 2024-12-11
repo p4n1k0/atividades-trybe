703e58c814bee34e9d1d44a843da3f97fb220137
p4n1k0 <gabre.tarick@gmail.com>
2024-12-11 18:33:06 -0300

    rate limit

--- 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/requisicoes-web/requisicao.py
+++ 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/requisicoes-web/requisicao.py
@@ -1,34 +1,10 @@
 import requests
+import time
 
 
-# Requisição do tipo GET
-response = requests.get("https://www.betrybe.com/")
-print(response.status_code)  # código de status
-print(response.headers["Content-Type"])  # conteúdo no formato html
-
-# Conteúdo recebido da requisição
-print(response.text)
-
-# Bytes recebidos como resposta
-print(response.content)
-
-# Requisição do tipo post
-response = requests.post("http://httpbin.org/post", data="some content")
-print(response.text)
-
-# Requisição enviando cabeçalho (header)
-response = requests.get("http://httpbin.org/get", headers={"Accept": "application/json"})
-print(response.text)
-
-# Requisição a recurso binário
-response = requests.get("http://httpbin.org/image/png")
-print(response.content)
-
-# Recurso JSON
-response = requests.get("http://httpbin.org/get")
-# Equivalente ao json.loads(response.content)
-print(response.json())
-
-# Podemos também pedir que a resposta lance uma exceção caso o status não seja OK
-response = requests.get("http://httpbin.org/status/404")
-response.raise_for_status()
+# Coloca uma pausa de 6 segundos a cada requisição
+# Obs: este site de exemplo tem um rate limit de 10 requisições por minuto
+for _ in range(15):
+    response = requests.get("https://www.cloudflare.com/rate-limit-test/")
+    print(response)
+    time.sleep(6)
