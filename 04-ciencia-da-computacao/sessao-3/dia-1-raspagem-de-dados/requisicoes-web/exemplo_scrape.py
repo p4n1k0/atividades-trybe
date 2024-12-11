8beefdb1b3770d5eccf27794880a00a97049aa41
p4n1k0 <gabre.tarick@gmail.com>
2024-12-11 18:54:23 -0300

    limpeza de dados

--- 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/requisicoes-web/exemplo_scrape.py
+++ 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/requisicoes-web/exemplo_scrape.py
@@ -12,7 +12,23 @@ while next_page_url:
     # Imprime os produtos de uma determinada página
     for product in selector.css(".product_pod"):
         title = product.css("h3 a::attr(title)").get()
-        price = product.css(".price_color::text").get()
+        price = product.css(".product_price .price_color::text").re(r"£\d+\.\d{2}")
         print(title, price)
+        
+        # Busca o detalhe de um produto
+        detail_href = product.css("h3 a::attr(href)").get()
+        detail_page_url = URL_BASE + detail_href
+
+        # Baixa o conteúdo da página de detalhes
+        detail_response = requests.get(detail_page_url)
+        detail_selector = Selector(text=detail_response.text)
+
+        # Extrai a descrição do produto
+        description = detail_selector.css("#product_description ~ p::text").get()
+        suffix = "...more"
+        if description.endswith(suffix):
+            description = description[:-len(suffix)]
+        print(description)
+        
     # Descobre qual é a próxima página
     next_page_url = selector.css(".next a::attr(href)").get()
