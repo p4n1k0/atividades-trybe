410c177bff9fdb7291d8c11394bf724eac36910d
p4n1k0 <gabre.tarick@gmail.com>
2024-12-12 14:26:51 -0300

    contexto with

--- 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/banco-de-dados/exemplo.py
+++ 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/banco-de-dados/exemplo.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
 
-client = MongoClient()
-# o banco de dados catalogue será criado se não existir
-db = client.catalogue
-# a coleção books será criada se não existir
-students = db.books
-client.close()  # fecha a conexão com o banco de dados
+
+with MongoClient() as client:
+    db = client.catalogue
+    for book in db.books.find({"title": {"$regex": "t"}}):
+        print(book["title"])
+        
