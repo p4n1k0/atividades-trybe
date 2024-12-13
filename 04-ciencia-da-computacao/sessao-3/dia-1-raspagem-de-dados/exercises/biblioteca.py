d5588e65a8964e2199f4c238567d55dd5e704e16
p4n1k0 <gabre.tarick@gmail.com>
2024-12-13 17:57:03 -0300

    calculo de livros publicados

--- 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/exercises/biblioteca.py
+++ 04-ciencia-da-computacao/sessao-3/dia-1-raspagem-de-dados/exercises/biblioteca.py
@@ -1,8 +1,13 @@
 from pymongo import MongoClient
 
 
-category = input("Escolha uma categoria: ")
 with MongoClient() as client:
     db = client.library
-    for book in db.books.find({"categories": category}, projection=["title"]):
-        print(book["title"])
+    pipelines = [
+        {"$match": {"status": "PUBLISH"}},
+        {"$unwind": "$categories"},
+        {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}
+    ]
+    for category in db.books.aggregate(pipelines):
+        print(category["_id"], category["count"], sep=": ")
