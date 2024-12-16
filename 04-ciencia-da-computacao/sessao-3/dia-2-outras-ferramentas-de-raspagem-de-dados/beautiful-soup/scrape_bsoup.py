dbcff5704933e533c3b3a2828442ef5d8f57ea3a
p4n1k0 <gabre.tarick@gmail.com>
2024-12-16 18:31:44 -0300

    link e title n funcionam

--- 04-ciencia-da-computacao/sessao-3/dia-2-outras-ferramentas-de-raspagem-de-dados/beautiful-soup/scrape_bsoup.py
+++ 04-ciencia-da-computacao/sessao-3/dia-2-outras-ferramentas-de-raspagem-de-dados/beautiful-soup/scrape_bsoup.py
@@ -14,30 +14,13 @@ def get_html_soup(url):
 
 # Recebe um objeto soup e retorna informações das notícias de uma página
 def get_page_news(soup):
-
-    # Define uma lista vazia a ser populada com os dados do scraping
     news = []
 
-    # Percorre todos os elementos da tag 'article' com a classe especificada
     for post in soup.find_all(
         "article", {"class": "tec--card tec--card--medium"}
     ):
-
-        # Cria um dicionário para guardar os elementos a cada iteração
         item = {}
-
-        # Cria um item chamado tag no dicionário para guardar a tag do post
-        # Primeiro pesquisa pela div com a classe específica
-        # Depois pela tag 'a' dentro dos resultados do primeiro filtro
-        # Por fim, traz o resultado da string dentro da tag a
         item["tag"] = post.find("div", {"class": "tec--card__info"}).a.string
-
-        # Mesma lógica da busca anterior
-        item["title"] = post.find("h3", {"class": "tec--card__title"}).a.string
-
-        # Parecido com o que foi feito anteriormente, mas dessa vez pega
-        # o atributo 'href' dentro da tag 'a'
-        item["link"] = post.find("h3", {"class": "tec--card__title"}).a["href"]
 
         # Mesma lógica da primeira busca, mas trazendo a string dentro da 'div'
         # direto
