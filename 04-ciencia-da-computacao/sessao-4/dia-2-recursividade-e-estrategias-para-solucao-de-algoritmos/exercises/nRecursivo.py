06d5d219a726b3f1dc57499962b169baaf349f7e
p4n1k0 <gabre.tarick@gmail.com>
2024-12-18 19:55:21 -0300

    transforma em recursiva

--- 04-ciencia-da-computacao/sessao-4/dia-2-recursividade-e-estrategias-para-solucao-de-algoritmos/exercises/nRecursivo.py
+++ 04-ciencia-da-computacao/sessao-4/dia-2-recursividade-e-estrategias-para-solucao-de-algoritmos/exercises/nRecursivo.py
@@ -1,6 +1,7 @@
 def conta_pares(n):
-    numero_de_pares = 0
-    for num in range(n+1):
-        if num % 2 == 0 and num != 0:
-            numero_de_pares += 1
-    return numero_de_pares
+    if n == 1:
+        return 0
+    elif n % 2 == 0:
+        return 1 + conta_pares(n-1)
+    else:
+        return conta_pares(n-1)
